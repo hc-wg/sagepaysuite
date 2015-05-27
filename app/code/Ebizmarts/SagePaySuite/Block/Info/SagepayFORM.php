eca60bf3abcffc43b54ada1c748cc58c9653e9d5
mosky17 <martin.gaibisso@gmail.com>
2015-05-27 16:05:20 -0300

    FORM basic structure

---
+++ app/code/Ebizmarts/SagePaySuite/Block/Info/SagepayFORM.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Copyright © 2015 eBizmarts. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+namespace Ebizmarts\SagePaySuite\Block\Info;
+
+class SagepayFORM extends \Magento\Payment\Block\Info
+{
+
+    /**
+    * @var string
+    */
+    protected $_template = 'Ebizmarts_SagePaySuite::info/sagepaysuite_form.phtml';
+
+}
