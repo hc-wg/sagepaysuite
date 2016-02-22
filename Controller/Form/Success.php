732995c35964b7975476dc4e7efefdd24bfecbe8
mosky17 <martin.gaibisso@gmail.com>
2016-02-22 18:01:28 -0300

    TEST: Form tests for 2 controllers and minor fixes.

--- Controller/Form/Success.php
+++ Controller/Form/Success.php
@@ -58,16 +58,15 @@ class Success extends \Magento\Framework\App\Action\Action
     protected $_crypt;
 
     /**
-     * Success constructor.
      * @param \Magento\Framework\App\Action\Context $context
      * @param \Magento\Customer\Model\Session $customerSession
      * @param \Magento\Checkout\Model\Session $checkoutSession
      * @param \Ebizmarts\SagePaySuite\Model\Config $config
-     * @param \Magento\Checkout\Helper\Data $checkoutData
-     * @param \Magento\Quote\Model\QuoteManagement $quoteManagement
-     * @param OrderSender $orderSender
      * @param \Psr\Log\LoggerInterface $logger
+     * @param Logger $suiteLogger
      * @param \Magento\Sales\Model\Order\Payment\TransactionFactory $transactionFactory
+     * @param \Ebizmarts\SagePaySuite\Helper\Checkout $checkoutHelper
+     * @param \Crypt_AES $crypt
      */
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
@@ -103,17 +102,16 @@ class Success extends \Magento\Framework\App\Action\Action
     {
         try {
 
-            $response = $this->decodeSagePayResponse($this->getRequest()->getParam("crypt"));
+            $response = $this->_decodeSagePayResponse($this->getRequest()->getParam("crypt"));
 
             //log response
             $this->_suiteLogger->SageLog(Logger::LOG_REQUEST, $response);
 
-            $this->_quote = $this->_getCheckoutSession()->getQuote();
-
-            $this->_quote->save();
+            $this->_quote = $this->_checkoutSession->getQuote();
+            //$this->_quote->save();
 
             $transactionId = $response["VPSTxId"];
-            $transactionId = str_replace("{","",str_replace("}","",$transactionId)); //strip brackets
+            $transactionId = str_replace("{", "", str_replace("}", "", $transactionId)); //strip brackets
 
             //import payment info for save order
             $payment = $this->_quote->getPayment();
@@ -122,12 +120,12 @@ class Success extends \Magento\Framework\App\Action\Action
             $payment->setLastTransId($transactionId);
             $payment->setCcType($response["CardType"]);
             $payment->setCcLast4($response["Last4Digits"]);
-            if(array_key_exists("ExpiryDate",$response)){
-                $payment->setCcExpMonth(substr($response["ExpiryDate"],0,2));
-                $payment->setCcExpYear(substr($response["ExpiryDate"],2));
+            if (array_key_exists("ExpiryDate", $response)) {
+                $payment->setCcExpMonth(substr($response["ExpiryDate"], 0, 2));
+                $payment->setCcExpYear(substr($response["ExpiryDate"], 2));
             }
-            if(array_key_exists("3DSecureStatus",$response)){
-                $payment->setAdditionalInformation('threeDStatus',$response["3DSecureStatus"]);
+            if (array_key_exists("3DSecureStatus", $response)) {
+                $payment->setAdditionalInformation('threeDStatus', $response["3DSecureStatus"]);
             }
             $payment->setAdditionalInformation('statusDetail', $response["StatusDetail"]);
             $payment->setAdditionalInformation('vendorTxCode', $response["VendorTxCode"]);
@@ -139,13 +137,15 @@ class Success extends \Magento\Framework\App\Action\Action
             $quoteId = $this->_quote->getId();
 
             //prepare session to success or cancellation page
-            $this->_getCheckoutSession()->clearHelperData();
-            $this->_getCheckoutSession()->setLastQuoteId($quoteId)->setLastSuccessQuoteId($quoteId);
+            $this->_checkoutSession->clearHelperData();
+            $this->_checkoutSession->setLastQuoteId($quoteId);
+            $this->_checkoutSession->setLastSuccessQuoteId($quoteId);
+
             //an order may be created
             if ($order) {
-                $this->_getCheckoutSession()->setLastOrderId($order->getId())
-                    ->setLastRealOrderId($order->getIncrementId())
-                    ->setLastOrderStatus($order->getStatus());
+                $this->_checkoutSession->setLastOrderId($order->getId());
+                $this->_checkoutSession->setLastRealOrderId($order->getIncrementId());
+                $this->_checkoutSession->setLastOrderStatus($order->getStatus());
 
                 //send email
                 $this->_checkoutHelper->sendOrderEmail($order);
@@ -157,8 +157,7 @@ class Success extends \Magento\Framework\App\Action\Action
             $payment->setIsTransactionClosed(1);
             $payment->save();
 
-            switch($this->_config->getSagepayPaymentAction())
-            {
+            switch ($this->_config->getSagepayPaymentAction()) {
                 case \Ebizmarts\SagePaySuite\Model\Config::ACTION_PAYMENT:
                     $action = \Magento\Sales\Model\Order\Payment\Transaction::TYPE_CAPTURE;
                     $closed = true;
@@ -178,18 +177,18 @@ class Success extends \Magento\Framework\App\Action\Action
             }
 
             //create transaction record
-            $transaction = $this->_transactionFactory->create()
-                ->setOrderPaymentObject($payment)
-                ->setTxnId($transactionId)
-                ->setOrderId($order->getEntityId())
-                ->setTxnType($action)
-                ->setPaymentId($payment->getId());
+            $transaction = $this->_transactionFactory->create();
+            $transaction->setOrderPaymentObject($payment);
+            $transaction->setTxnId($transactionId);
+            $transaction->setOrderId($order->getEntityId());
+            $transaction->setTxnType($action);
+            $transaction->setPaymentId($payment->getId());
             $transaction->setIsClosed($closed);
             $transaction->save();
 
             //update invoice transaction id
             $invoices = $order->getInvoiceCollection();
-            if($invoices->count()){
+            if (!empty($invoices)) {
                 foreach ($invoices as $_invoice) {
                     $_invoice->setTransactionId($payment->getLastTransId());
                     $_invoice->save();
@@ -200,9 +199,10 @@ class Success extends \Magento\Framework\App\Action\Action
 
             return;
 
-        } catch (\Exception $e) {
+        } catch (\Exception $e)
+        {
             $this->_logger->critical($e);
-            $this->_redirectToCartAndShowError('We can\'t place the order. Please try another payment method.');
+            $this->_redirectToCartAndShowError('We can\'t place the order.');
         }
     }
 
@@ -218,29 +218,31 @@ class Success extends \Magento\Framework\App\Action\Action
         $this->_redirect('checkout/cart');
     }
 
-    protected function decodeSagePayResponse($crypt){
+    protected function _decodeSagePayResponse($crypt)
+    {
         if (empty($crypt)) {
             $this->_redirectToCartAndShowError('Invalid response from SagePay, please contact our support team to rectify payment.');
-        }else{
-            $strDecoded = $this->decrypt($crypt);
+        } else {
+            $strDecoded = $this->_decrypt($crypt);
 
-            $responseRaw = explode('&',$strDecoded);
+            $responseRaw = explode('&', $strDecoded);
             $response = array();
 
-            for($i = 0;$i < count($responseRaw);$i++){
-                $strField = explode('=',$responseRaw[$i]);
+            for ($i = 0; $i < count($responseRaw); $i++) {
+                $strField = explode('=', $responseRaw[$i]);
                 $response[$strField[0]] = $strField[1];
             }
 
-            if(!array_key_exists(\Ebizmarts\SagePaySuite\Model\Config::VAR_VPSTxId,$response)){
+            if (!array_key_exists("VPSTxId", $response)) {
                 $this->_redirectToCartAndShowError('Invalid response from SagePay, please contact our support team to rectify payment.');
-            }else{
+            } else {
                 return $response;
             }
         }
     }
 
-    public function decrypt($strIn) {
+    protected function _decrypt($strIn)
+    {
         $cryptPass = $this->_config->getFormEncryptedPassword();
 
         //** remove the first char which is @ to flag this is AES encrypted
@@ -256,18 +258,4 @@ class Success extends \Magento\Framework\App\Action\Action
 
         return $decoded;
     }
-
-    protected function _getCheckoutSession()
-    {
-        return $this->_checkoutSession;
-    }
-
-    /**
-     * @return \Magento\Checkout\Model\Session
-     */
-    protected function _getCustomerSession()
-    {
-        return $this->_customerSession;
-    }
-
 }
---
+++ Test/Unit/Controller/Form/SuccessTest.php
@@ -0,0 +1,186 @@
+<?php
+/**
+ * Copyright © 2015 ebizmarts. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+namespace Ebizmarts\SagePaySuite\Test\Unit\Controller\Form;
+
+use Magento\Framework\TestFramework\Unit\Helper\ObjectManager as ObjectManagerHelper;
+
+class SuccessTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Sage Pay Transaction ID
+     */
+    const TEST_VPSTXID = 'F81FD5E1-12C9-C1D7-5D05-F6E8C12A526F';
+
+    /**
+     * @var /Ebizmarts\SagePaySuite\Controller\Paypal\Callback
+     */
+    protected $formSuccessController;
+
+    /**
+     * @var RequestInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $requestMock;
+
+    /**
+     * @var Http|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $responseMock;
+
+    /**
+     * @var \Magento\Framework\App\Response\RedirectInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $redirectMock;
+
+    /**
+     * @var  Magento\Sales\Model\Order|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $orderMock;
+
+    protected function setUp()
+    {
+        $paymentMock = $this
+            ->getMockBuilder('Magento\Sales\Model\Order\Payment')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $quoteMock = $this
+            ->getMockBuilder('Magento\Quote\Model\Quote')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $quoteMock->expects($this->any())
+            ->method('getPayment')
+            ->will($this->returnValue($paymentMock));
+
+        $checkoutSessionMock = $this
+            ->getMockBuilder('Magento\Checkout\Model\Session')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $checkoutSessionMock->expects($this->any())
+            ->method('getQuote')
+            ->will($this->returnValue($quoteMock));
+
+        $this->responseMock = $this
+            ->getMock('Magento\Framework\App\Response\Http', [], [], '', false);
+
+        $this->requestMock = $this
+            ->getMockBuilder('Magento\Framework\HTTP\PhpEnvironment\Request')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->redirectMock = $this->getMockForAbstractClass('Magento\Framework\App\Response\RedirectInterface');
+
+        $messageManagerMock = $this->getMockBuilder('Magento\Framework\Message\ManagerInterface')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $contextMock = $this->getMockBuilder('Magento\Framework\App\Action\Context')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $contextMock->expects($this->any())
+            ->method('getRequest')
+            ->will($this->returnValue($this->requestMock));
+        $contextMock->expects($this->any())
+            ->method('getResponse')
+            ->will($this->returnValue($this->responseMock));
+        $contextMock->expects($this->any())
+            ->method('getRedirect')
+            ->will($this->returnValue($this->redirectMock));
+        $contextMock->expects($this->any())
+            ->method('getMessageManager')
+            ->will($this->returnValue($messageManagerMock));
+
+        $configMock = $this
+            ->getMockBuilder('Ebizmarts\SagePaySuite\Model\Config')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->orderMock = $this
+            ->getMockBuilder('Magento\Sales\Model\Order')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->orderMock->expects($this->any())
+            ->method('getPayment')
+            ->will($this->returnValue($paymentMock));
+
+        $transactionMock = $this
+            ->getMockBuilder('Magento\Sales\Model\Order\Payment\Transaction')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $transactionFactoryMock = $this
+            ->getMockBuilder('Magento\Sales\Model\Order\Payment\TransactionFactory')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $transactionFactoryMock->expects($this->any())
+            ->method('create')
+            ->will($this->returnValue($transactionMock));
+
+        $checkoutHelperMock = $this
+            ->getMockBuilder('Ebizmarts\SagePaySuite\Helper\Checkout')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $checkoutHelperMock->expects($this->any())
+            ->method('placeOrder')
+            ->will($this->returnValue($this->orderMock));
+
+        $cryptMock = $this
+            ->getMockBuilder('Crypt_AES')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $cryptMock->expects($this->any())
+            ->method('decrypt')
+            ->will($this->returnValue("decodedCrypt=true&VPSTxId={" . self::TEST_VPSTXID . "}" .
+                "&CardType=VISA&Last4Digits=0006&StatusDetail=OK_STATUS_DETAIL" .
+                "&VendorTxCode=100000001-2016-12-12-12346789"));
+
+        $objectManagerHelper = new ObjectManagerHelper($this);
+        $this->formSuccessController = $objectManagerHelper->getObject(
+            'Ebizmarts\SagePaySuite\Controller\Form\Success',
+            [
+                'context' => $contextMock,
+                'config' => $configMock,
+                'checkoutSession' => $checkoutSessionMock,
+                'checkoutHelper' => $checkoutHelperMock,
+                'transactionFactory' => $transactionFactoryMock,
+                'crypt' => $cryptMock
+            ]
+        );
+    }
+
+    public function testExecuteSUCCESS()
+    {
+        $this->requestMock->expects($this->once())
+            ->method('getParam')
+            ->with('crypt')
+            ->will($this->returnValue("213354325435454542354324532"));
+
+        $this->_expectRedirect("checkout/onepage/success");
+        $this->formSuccessController->execute();
+    }
+
+    public function testExecuteERROR()
+    {
+        $this->requestMock->expects($this->once())
+            ->method('getParam')
+            ->with('crypt')
+            ->will($this->returnValue("fdsfds87dfs87dfs78dfs"));
+
+        $this->_expectRedirect("checkout/cart");
+        $this->formSuccessController->execute();
+    }
+
+    /**
+     * @param string $path
+     */
+    protected function _expectRedirect($path)
+    {
+        $this->redirectMock->expects($this->once())
+            ->method('redirect')
+            ->with($this->anything(), $path, []);
+    }
+}
