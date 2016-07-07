1baba6d46c6fd8c5cdd4854f7979456a6b9d6be7
centerax <pablo@ebizmarts.com>
2016-07-07 10:21:25 -0300

    change param type and add getQuoteById method

--- Api/ServerManagementInterface.php
+++ Api/ServerManagementInterface.php
@@ -12,12 +12,18 @@ interface ServerManagementInterface
     /**
      * Set payment information and place order for a specified cart.
      *
-     * @param int $cartId
+     * @param mixed $cartId
      * @param bool $save_token
      * @param string $token
      * @throws \Magento\Framework\Exception\CouldNotSaveException
      * @return \Ebizmarts\SagePaySuite\Api\Data\ResultInterface
      */
     public function savePaymentInformationAndPlaceOrder($cartId, $save_token, $token);
+
+    /**
+     * @param mixed $cartId
+     * @return \Magento\Quote\Api\Data\CartInterface
+     */
+    public function getQuoteById($cartId);
 
 }
