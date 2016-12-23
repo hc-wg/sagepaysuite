bf8e056c67dab7662f1a88197e5d73d5a5c5bf3e
centerax <pablo@ebizmarts.com>
2016-12-23 10:47:09 -0300

    more tests

--- Test/Unit/Model/Api/PIRestTest.php
+++ Test/Unit/Model/Api/PIRestTest.php
@@ -363,4 +363,99 @@ class PIRestTest extends \PHPUnit_Framework_TestCase
             );
         }
     }
+
+    public function testVoidSucess()
+    {
+        $this->curlMock->expects($this->once())
+            ->method('read')
+            ->willReturn(
+                'Content-Language: en-GB' . PHP_EOL . PHP_EOL .
+                '{"instructionType": "void","date": "2015-08-11T11:45:16.285+01:00"}'
+            );
+
+        $this->curlMock->expects($this->once())
+            ->method('getInfo')
+            ->willReturn(201);
+
+        $this->curlMock->expects($this->once())
+            ->method('write')
+            ->with(
+                \Zend_Http_Client::POST,
+                \Ebizmarts\SagePaySuite\Model\Config::URL_PI_API_TEST .
+                "transactions/2B97808F-9A36-6E71-F87F-6714667E8AF4/instructions",
+                '1.0',
+                ['Content-type: application/json'],
+                '{"instructionType":"void"}'
+            );
+
+        $result = $this->pirestApiModel->void("2B97808F-9A36-6E71-F87F-6714667E8AF4");
+        $this->assertEquals($result->instructionType, "void");
+        $this->assertEquals($result->date, "2015-08-11T11:45:16.285+01:00");
+    }
+
+    public function testRefundSucess()
+    {
+        $this->curlMock->expects($this->once())
+            ->method('read')
+            ->willReturn(
+                'Content-Language: en-GB' . PHP_EOL . PHP_EOL .
+                '{
+                    "statusCode": "0000",
+                    "statusDetail": "The Authorisation was Successful.",
+                    "transactionId": "043D6711-E722-ACC6-2C2E-B03E00BF7603",
+                    "transactionType": "Refund",
+                    "retrievalReference": 13551640,
+                    "bankAuthorisationCode": "999778",
+                    "paymentMethod": {
+                        "card": {
+                            "cardType": "MasterCard",
+                            "lastFourDigits": "0001",
+                            "expiryDate": "0520"
+                        }
+                    },
+                    "status": "Ok"
+                }'
+            );
+
+        $this->curlMock->expects($this->once())
+            ->method('getInfo')
+            ->willReturn(201);
+
+        $write = preg_replace('/\s\s+/', '', str_replace(array("\r","\n"), '', '{
+                    "transactionType":"Refund",
+                    "vendorTxCode":"R000000122-2016-12-22-1423481482416628",
+                    "referenceTransactionId":"2B97808F-9A36-6E71-F87F-6714667E8AF4",
+                    "amount":10800,
+                    "currency":"GBP",
+                    "description":"Magento backend refund."
+                 }'));
+
+        $this->curlMock->expects($this->once())
+            ->method('write')
+            ->with(
+                \Zend_Http_Client::POST,
+                \Ebizmarts\SagePaySuite\Model\Config::URL_PI_API_TEST .
+                \Ebizmarts\SagePaySuite\Model\Api\PIRest::ACTION_TRANSACTIONS,
+                '1.0',
+                ['Content-type: application/json'],
+                $write
+            );
+
+        $result = $this->pirestApiModel->refund(
+            "R000000122-2016-12-22-1423481482416628",
+            "2B97808F-9A36-6E71-F87F-6714667E8AF4",
+            10800,
+            "GBP",
+            "Magento backend refund."
+        );
+
+        $this->assertEquals($result->transactionType, "Refund");
+        $this->assertEquals($result->transactionId, "043D6711-E722-ACC6-2C2E-B03E00BF7603");
+        $this->assertEquals($result->statusCode, "0000");
+        $this->assertEquals($result->statusDetail, "The Authorisation was Successful.");
+        $this->assertEquals($result->retrievalReference, "13551640");
+        $this->assertEquals($result->bankAuthorisationCode, "999778");
+        $this->assertEquals($result->status, "Ok");
+        $this->assertObjectHasAttribute("paymentMethod", $result);
+    }
 }
