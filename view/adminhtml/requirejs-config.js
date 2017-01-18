1828b8eb58c92a2ce85f15001dacfd641127c185
centerax <pablo@ebizmarts.com>
2017-01-18 16:10:00 -0300

    add dropin source files to head

--- view/adminhtml/requirejs-config.js
+++ view/adminhtml/requirejs-config.js
@@ -8,8 +8,9 @@ var config = {
         '*': {
             sagepayjs_live: 'https://live.sagepay.com/api/v1/js/sagepay.js',
             sagepayjs_test: 'https://test.sagepay.com/api/v1/js/sagepay.js',
+            sagepayjs_dropin_test: 'https://test.sagepay.com/api/v1/js/sagepay-dropin.js',
+            sagepayjs_dropin_live: 'https://live.sagepay.com/api/v1/js/sagepay-dropin.js',
             sagepaysuitepiCcForm: 'Ebizmarts_SagePaySuite/js/pi-cc-form',
-            //sagepaysuiteServerForm: 'Ebizmarts_SagePaySuite/js/server-form',
             sagepaysuiteFormForm: 'Ebizmarts_SagePaySuite/js/form-form',
             sagepaysuiteRepeatForm: 'Ebizmarts_SagePaySuite/js/repeat-form'
         }
