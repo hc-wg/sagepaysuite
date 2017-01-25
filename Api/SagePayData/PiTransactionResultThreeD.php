2c25dcccf0d9ebdd5f2ee706672472e5ae2ee8d6
centerax <pablo@ebizmarts.com>
2017-01-25 18:37:26 -0300

    impl of PiTransactionResultThreeDInterface

---
+++ Api/SagePayData/PiTransactionResultThreeD.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pablo
+ * Date: 1/25/17
+ * Time: 6:20 PM
+ */
+
+namespace Ebizmarts\SagePaySuite\Api\SagePayData;
+
+
+class PiTransactionResultThreeD extends \Magento\Framework\Api\AbstractExtensibleObject implements \Ebizmarts\SagePaySuite\Api\SagePayData\PiTransactionResultThreeDInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function getStatus()
+    {
+        return $this->_get(self::STATUS);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setStatus($status)
+    {
+        $this->setData(self::STATUS, $status);
+    }
+}
