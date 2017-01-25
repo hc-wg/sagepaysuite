aa6c6c71a76b218ea1bdd87634e28b46136ec3ad
centerax <pablo@ebizmarts.com>
2017-01-25 17:59:41 -0300

    new files. interfeaces and imps

---
+++ Api/SagePayData/PiTransactionResultPaymentMethod.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pablo
+ * Date: 1/25/17
+ * Time: 4:01 PM
+ */
+
+namespace Ebizmarts\SagePaySuite\Api\SagePayData;
+
+
+class PiTransactionResultPaymentMethod extends \Magento\Framework\Api\AbstractExtensibleObject implements \Ebizmarts\SagePaySuite\Api\SagePayData\PiTransactionResultPaymentMethodInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function getCard()
+    {
+        return $this->_get(self::CARD);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setCard($card)
+    {
+        $this->setData(self::CARD, $card);
+    }
+}
