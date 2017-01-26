6c880047ff302e9f38d144d6be414dac4a901d10
centerax <pablo@ebizmarts.com>
2017-01-26 17:12:59 -0300

    new interfaces

---
+++ Api/SagePayData/PiThreeDSecureRequest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pablo
+ * Date: 1/26/17
+ * Time: 3:20 PM
+ */
+
+namespace Ebizmarts\SagePaySuite\Api\SagePayData;
+
+
+class PiThreeDSecureRequest extends \Magento\Framework\Api\AbstractExtensibleObject implements PiThreeDSecureRequestInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function getParEs()
+    {
+        return $this->_get(self::PAR_ES);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setParEs($message)
+    {
+        $this->setData(self::PAR_ES, $message);
+    }
+}
