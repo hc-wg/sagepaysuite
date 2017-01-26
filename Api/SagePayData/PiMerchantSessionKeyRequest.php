5e68326f21caddb04a693cd5d968ee1bc16247d9
centerax <pablo@ebizmarts.com>
2017-01-26 15:11:57 -0300

    Merchant session key request and response interfaces.

---
+++ Api/SagePayData/PiMerchantSessionKeyRequest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pablo
+ * Date: 1/26/17
+ * Time: 1:54 PM
+ */
+
+namespace Ebizmarts\SagePaySuite\Api\SagePayData;
+
+
+class PiMerchantSessionKeyRequest extends \Magento\Framework\Api\AbstractExtensibleObject implements PiMerchantSessionKeyRequestInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function getVendorName()
+    {
+        return $this->_get(self::VENDOR_NAME);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setVendorName($name)
+    {
+        $this->setData(self::VENDOR_NAME, $name);
+    }
+}
