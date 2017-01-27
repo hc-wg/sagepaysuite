92d3e35765f3d557a8765972acaddac15db29724
centerax <pablo@ebizmarts.com>
2017-01-27 10:24:31 -0300

    Adding sagepaydata interfaces.

---
+++ Api/SagePayData/PiInstructionResponse.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Ebizmarts\SagePaySuite\Api\SagePayData;
+
+
+class PiInstructionResponse extends \Magento\Framework\Api\AbstractExtensibleObject implements PiInstructionResponseInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function getInstructionType()
+    {
+        return $this->_get(self::INSTRUCTION_TYPE);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setInstructionType($instructionType)
+    {
+        $this->setData(self::INSTRUCTION_TYPE, $instructionType);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getDate()
+    {
+        return $this->_get(self::DATE);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setDate($date)
+    {
+        $this->setData(self::DATE, $date);
+    }
+}
