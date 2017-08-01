954f3769b636978d878dbae83f4c94da8b5e72cd
centerax <pablo@ebizmarts.com>
2017-08-01 11:02:21 -0300

    adding order rollback

--- Test/Api/_files/quote_with_sagepaysuiteserver_payment_rollback.php
+++ Test/Api/_files/quote_with_sagepaysuiteserver_payment_rollback.php
@@ -1,10 +1,7 @@
 <?php
-/**
- * Copyright © 2013-2017 Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
 
 require __DIR__ . '/../../../../../dev/tests/integration/testsuite/Magento/Catalog/_files/products_rollback.php';
 require __DIR__ . '/../../../../../dev/tests/integration/testsuite/Magento/Customer/_files/customer_address_rollback.php';
 require __DIR__ . '/../../../../../dev/tests/integration/testsuite/Magento/Customer/_files/customer_rollback.php';
 require __DIR__ . '/../../../../../dev/tests/integration/testsuite/Magento/Checkout/_files/active_quote_rollback.php';
+require __DIR__ . '/../../../../../dev/tests/integration/testsuite/Magento/Sales/_files/default_rollback.php';
