e5317877fcd1d942109316ed2fe938ddf0a050ad
centerax <pablo@ebizmarts.com>
2017-08-03 13:02:26 -0300

    reuse code with Helper

--- Test/Api/MerchantSessionKeyTest.php
+++ Test/Api/MerchantSessionKeyTest.php
@@ -8,29 +8,29 @@ use Magento\TestFramework\TestCase\WebapiAbstract;
 
 class MerchantSessionKeyTest extends WebapiAbstract
 {
-    const TEST_API_KEY = "snEEZ7EFaM5q9GzBspep";
-    const TEST_API_PASSWORD = "MrzrB8u3CST4FLLNRXL6";
     const VALID_MERCHANT_SESSION_KEY = "/^[A-F0-9]{8}-[A-F0-9]{4}-[A-F0-9]{4}-[A-F0-9]{4}-[A-F0-9]{12}$/";
 
     /** @var \Magento\Framework\ObjectManagerInterface */
     private $objectManager;
 
-    /** @var \Magento\Config\Model\Config */
-    private $config;
+    /** @var  \Ebizmarts\SagePaySuite\Test\API\Helper */
+    private $helper;
 
     protected function setUp()
     {
-        $this->config = Bootstrap::getObjectManager()->create(
+        $this->objectManager = Bootstrap::getObjectManager();
+
+        $this->config = $this->objectManager->create(
             'Magento\Config\Model\Config'
         );
 
-        $this->objectManager = Bootstrap::getObjectManager();
+        $this->helper = $this->objectManager->create("Ebizmarts\SagePaySuite\Test\API\Helper");
     }
 
     public function testMskCall()
     {
-        $this->savePiKey();
-        $this->savePiPassword();
+        $this->helper->savePiKey();
+        $this->helper->savePiPassword();
 
         $serviceInfo = [
             'rest' => [
@@ -45,20 +45,4 @@ class MerchantSessionKeyTest extends WebapiAbstract
         $this->assertRegExp(self::VALID_MERCHANT_SESSION_KEY, $response['response']);
     }
 
-    private function savePiKey()
-    {
-        $this->config->setDataByPath("payment/sagepaysuitepi/key", self::TEST_API_KEY);
-        $this->config->save();
-    }
-
-    private function savePiPassword()
-    {
-        $model = $this->objectManager->create('Magento\Config\Model\Config\Backend\Encrypted');
-        $model->setPath('payment/sagepaysuitepi/password');
-        $model->setScopeId(0);
-        $model->setScope('default');
-        $model->setScopeCode('');
-        $model->setValue(self::TEST_API_PASSWORD);
-        $model->save();
-    }
 }
