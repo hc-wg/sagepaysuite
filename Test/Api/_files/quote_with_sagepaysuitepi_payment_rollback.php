0fd3c8103d577af1c1e9baf60249457270c7092b
centerax <pablo@ebizmarts.com>
2017-08-03 16:47:59 -0300

    Fix base path

--- Test/Api/_files/quote_with_sagepaysuitepi_payment_rollback.php
+++ Test/Api/_files/quote_with_sagepaysuitepi_payment_rollback.php
@@ -1,7 +1,7 @@
 <?php
 
-require __DIR__ . '/../../../../../dev/tests/integration/testsuite/Magento/Catalog/_files/products_rollback.php';
-require __DIR__ . '/../../../../../dev/tests/integration/testsuite/Magento/Customer/_files/customer_address_rollback.php';
-require __DIR__ . '/../../../../../dev/tests/integration/testsuite/Magento/Customer/_files/customer_rollback.php';
-require __DIR__ . '/../../../../../dev/tests/integration/testsuite/Magento/Checkout/_files/active_quote_rollback.php';
-require __DIR__ . '/../../../../../dev/tests/integration/testsuite/Magento/Sales/_files/default_rollback.php';
+require BP . '/dev/tests/integration/testsuite/Magento/Catalog/_files/products_rollback.php';
+require BP . '/dev/tests/integration/testsuite/Magento/Customer/_files/customer_address_rollback.php';
+require BP . '/dev/tests/integration/testsuite/Magento/Customer/_files/customer_rollback.php';
+require BP . '/dev/tests/integration/testsuite/Magento/Checkout/_files/active_quote_rollback.php';
+require BP . '/dev/tests/integration/testsuite/Magento/Sales/_files/default_rollback.php';
