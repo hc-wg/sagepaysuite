bb016d6f210f29c0591a9dc63f37d0eadf5814e0
centerax <pablo@ebizmarts.com>
2017-09-28 09:24:50 -0300

    TokenListTest update to phpunit 6

--- Test/Unit/Block/Customer/TokenListTest.php
+++ Test/Unit/Block/Customer/TokenListTest.php
@@ -17,40 +17,14 @@ class TokenListTest extends \PHPUnit\Framework\TestCase
 
     public function testGetBackUrl()
     {
-        $urlBuilderMock = $this
-            ->getMockBuilder('Magento\Framework\UrlInterface')
-            ->disableOriginalConstructor()
-            ->getMock();
+        $urlBuilderMock = $this->makeUrlBuilderMockWithGetUrl();
         $urlBuilderMock->expects($this->once())
             ->method('getUrl')
             ->with('customer/account/')
             ->willReturn('customer/account/');
 
-        $contextMock = $this->getMockBuilder('Magento\Framework\View\Element\Template\Context')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $contextMock->expects($this->any())
-            ->method('getUrlBuilder')
-            ->will($this->returnValue($urlBuilderMock));
-
-        $currentCustomerMock = $this
-            ->getMockBuilder('Magento\Customer\Helper\Session\CurrentCustomer')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $currentCustomerMock->expects($this->any())
-            ->method('getCustomerId')
-            ->will($this->returnValue(1));
-
-        $configMock = $this
-            ->getMockBuilder('Ebizmarts\SagePaySuite\Model\Config')
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $tokenModelMock = $this
-            ->getMockBuilder('Ebizmarts\SagePaySuite\Model\Token')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $contextMock->expects($this->any())
+        $tokenModelMock = $this->makeTokenModelMock();
+        $tokenModelMock->expects($this->any())
             ->method('getCustomerTokens')
             ->will($this->returnValue([]));
 
@@ -58,10 +32,10 @@ class TokenListTest extends \PHPUnit\Framework\TestCase
             ->setMethods(['setItems', 'getRefererUrl'])
             ->setConstructorArgs(
                 [
-                    "context"         => $contextMock,
-                    "currentCustomer" => $currentCustomerMock,
-                    "config"          => $configMock,
-                    "tokenModel"      => $tokenModelMock
+                    "context"         => $this->makeContextMockWithUrlBuilder($urlBuilderMock),
+                    "currentCustomer" => $this->makeCurrentCustomerMock(),
+                    "config"          => $this->makeConfigMock(),
+                    "tokenModel"      => $this->makeTokenModelMock()
                 ]
             )
             ->getMock();
@@ -75,38 +49,12 @@ class TokenListTest extends \PHPUnit\Framework\TestCase
 
     public function testGetBackUrlReferrer()
     {
-        $urlBuilderMock = $this
-            ->getMockBuilder('Magento\Framework\UrlInterface')
-            ->disableOriginalConstructor()
-            ->getMock();
+        $urlBuilderMock = $this->makeUrlBuilderMockWithGetUrl();
         $urlBuilderMock->expects($this->never())
             ->method('getUrl');
 
-        $contextMock = $this->getMockBuilder('Magento\Framework\View\Element\Template\Context')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $contextMock->expects($this->any())
-            ->method('getUrlBuilder')
-            ->will($this->returnValue($urlBuilderMock));
-
-        $currentCustomerMock = $this
-            ->getMockBuilder('Magento\Customer\Helper\Session\CurrentCustomer')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $currentCustomerMock->expects($this->any())
-            ->method('getCustomerId')
-            ->will($this->returnValue(1));
-
-        $configMock = $this
-            ->getMockBuilder('Ebizmarts\SagePaySuite\Model\Config')
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $tokenModelMock = $this
-            ->getMockBuilder('Ebizmarts\SagePaySuite\Model\Token')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $contextMock->expects($this->any())
+        $tokenModelMock = $this->makeTokenModelMock();
+        $tokenModelMock->expects($this->any())
             ->method('getCustomerTokens')
             ->will($this->returnValue([]));
 
@@ -114,9 +62,9 @@ class TokenListTest extends \PHPUnit\Framework\TestCase
             ->setMethods(['setItems', 'getRefererUrl'])
             ->setConstructorArgs(
                 [
-                    "context"         => $contextMock,
-                    "currentCustomer" => $currentCustomerMock,
-                    "config"          => $configMock,
+                    "context"         => $this->makeContextMockWithUrlBuilder($urlBuilderMock),
+                    "currentCustomer" => $this->makeCurrentCustomerMock(),
+                    "config"          => $this->makeConfigMock(),
                     "tokenModel"      => $tokenModelMock
                 ]
             )
@@ -127,5 +75,67 @@ class TokenListTest extends \PHPUnit\Framework\TestCase
         $url = $this->tokenListBlock->getBackUrl();
 
         $this->assertEquals('category/men.html', $url);
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    private function makeTokenModelMock()
+    {
+        $tokenModelMock = $this->getMockBuilder(\Ebizmarts\SagePaySuite\Model\Token::class)
+            ->disableOriginalConstructor()
+            ->setMethodsExcept(["saveToken"])
+            ->getMock();
+
+        return $tokenModelMock;
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    private function makeConfigMock()
+    {
+        $configMock = $this
+            ->getMockBuilder('Ebizmarts\SagePaySuite\Model\Config')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        return $configMock;
+    }
+
+    /**
+     * @param $urlBuilderMock
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    private function makeContextMockWithUrlBuilder($urlBuilderMock)
+    {
+        $contextMock = $this->getMockBuilder(\Magento\Framework\View\Element\Template\Context::class)
+            ->setMethods(["getUrlBuilder"])->disableOriginalConstructor()->getMock();
+        $contextMock->expects($this->any())->method('getUrlBuilder')->will($this->returnValue($urlBuilderMock));
+
+        return $contextMock;
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    private function makeCurrentCustomerMock(): \PHPUnit_Framework_MockObject_MockObject
+    {
+        $currentCustomerMock = $this->getMockBuilder('Magento\Customer\Helper\Session\CurrentCustomer')
+            ->setMethods(["getCustomerId"])->disableOriginalConstructor()->getMock();
+        $currentCustomerMock->expects($this->any())->method('getCustomerId')->will($this->returnValue(1));
+
+        return $currentCustomerMock;
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    private function makeUrlBuilderMockWithGetUrl()
+    {
+        $urlBuilderMock = $this->getMockBuilder(\Magento\Framework\Url::class)
+            ->setMethods(["getUrl"])->disableOriginalConstructor()->getMock();
+
+        return $urlBuilderMock;
     }
 }
