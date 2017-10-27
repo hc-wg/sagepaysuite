39b564e73d36fbccb832d9de0904eba5f41a649f
centerax <pablo@ebizmarts.com>
2017-10-27 10:19:11 -0300

    pass quote to getSessionKey

--- Api/PiMerchantInterface.php
+++ Api/PiMerchantInterface.php
@@ -11,7 +11,8 @@ interface PiMerchantInterface
     /**
      * Creates a merchant session key (MSK).
      *
+     * @param \Magento\Quote\Model\Quote $quote $quote
      * @return \Ebizmarts\SagePaySuite\Api\Data\ResultInterface
      */
-    public function getSessionKey();
+    public function getSessionKey(\Magento\Quote\Model\Quote $quote);
 }
