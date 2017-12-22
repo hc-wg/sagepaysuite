fe408af1eaf8a8ee442a07e94946caf8cefac81e
centerax <pablo@ebizmarts.com>
2017-12-22 12:10:59 -0300

    tests update

--- Setup/UpgradeSchema.php
+++ Setup/UpgradeSchema.php
@@ -6,6 +6,7 @@
 
 namespace Ebizmarts\SagePaySuite\Setup;
 
+use Magento\Framework\DB\Ddl\Table;
 use Magento\Framework\Setup\UpgradeSchemaInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
@@ -46,7 +47,7 @@ class UpgradeSchema implements UpgradeSchemaInterface
                     $setup->getTable('sales_payment_transaction'),
                 "sagepaysuite_fraud_check",
                 [
-                    'type' => \Magento\Framework\DB\Ddl\Table::TYPE_BOOLEAN,
+                    'type' => Table::TYPE_BOOLEAN,
                     'nullable' => false,
                     'comment' => 'Sage Pay Fraud Check Flag'
                 ]
@@ -61,22 +62,35 @@ class UpgradeSchema implements UpgradeSchemaInterface
      */
     private function createSagePaySuiteTokenTable(SchemaSetupInterface $installer)
     {
-        $table = $installer->getConnection()->newTable($installer->getTable('sagepaysuite_token'))->addColumn('id',
-                \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER, null,
+        $table = $installer
+            ->getConnection()
+            ->newTable($installer->getTable('sagepaysuite_token'))
+            ->addColumn(
+                'id',
+                Table::TYPE_INTEGER,
+                null,
                 ['nullable' => false, 'primary' => true, 'auto_increment' => true],
-                'Token Id')->addColumn('customer_id', \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER, null,
-                ['unsigned' => true, 'nullable' => false], 'Customer Id')->addColumn('token',
-                \Magento\Framework\DB\Ddl\Table::TYPE_TEXT, 150, ['nullable' => false], 'Token')->addColumn('cc_last_4',
-                \Magento\Framework\DB\Ddl\Table::TYPE_TEXT, 100, [], 'Cc Last 4')->addColumn('cc_exp_month',
-                \Magento\Framework\DB\Ddl\Table::TYPE_TEXT, 12, [], 'Cc Exp Month')->addColumn('cc_type',
-                \Magento\Framework\DB\Ddl\Table::TYPE_TEXT, 32, [], 'Cc Type')->addColumn('cc_exp_year',
-                \Magento\Framework\DB\Ddl\Table::TYPE_TEXT, 4, [], 'Cc Exp Year')->addColumn('vendorname',
-                \Magento\Framework\DB\Ddl\Table::TYPE_TEXT, 100, [], 'Vendorname')->addColumn('created_at',
-                \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP, null,
-                ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT],
-                'Created At')->addColumn('store_id', \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT, null,
-                ['unsigned' => true], 'Store Id')->addIndex($installer->getIdxName('sagepaysuite_token',
-                ['customer_id']), ['customer_id']);
+                'Token Id'
+            )
+            ->addColumn('customer_id', Table::TYPE_INTEGER, null,
+                ['unsigned' => true, 'nullable' => false], 'Customer Id')
+            ->addColumn('token', Table::TYPE_TEXT, 150,
+                ['nullable' => false], 'Token')
+            ->addColumn('cc_last_4', Table::TYPE_TEXT, 100, [],
+                'Cc Last 4')
+            ->addColumn('cc_exp_month', Table::TYPE_TEXT, 12, [], 'Cc Exp Month')
+            ->addColumn('cc_type',
+                Table::TYPE_TEXT, 32, [], 'Cc Type')
+            ->addColumn('cc_exp_year', Table::TYPE_TEXT, 4, [],
+                'Cc Exp Year')
+            ->addColumn('cc_exp_year', Table::TYPE_TEXT, 100, [],
+                'Vendorname')
+            ->addColumn('created_at', Table::TYPE_TIMESTAMP, null,
+                ['nullable' => false, 'default' => Table::TIMESTAMP_INIT], 'Created At')
+            ->addColumn('store_id',
+                Table::TYPE_SMALLINT, null, ['unsigned' => true],
+                'Store Id')
+            ->addIndex($installer->getIdxName('sagepaysuite_token', ['customer_id']), ['customer_id']);
 
         $installer->getConnection()->createTable($table);
     }
