b50aa438d588f05af2c6555038d001937fd3a66b
centerax <pablos.benitez@gmail.com>
2018-03-05 17:56:59 -0300

    Test update. RulesTest

--- Test/Unit/Block/Adminhtml/Template/Reports/Fraud/Grid/Renderer/RulesTest.php
+++ Test/Unit/Block/Adminhtml/Template/Reports/Fraud/Grid/Renderer/RulesTest.php
@@ -16,15 +16,42 @@ class RulesTest extends \PHPUnit\Framework\TestCase
     // @codingStandardsIgnoreStart
     protected function setUp()
     {
+        $objectManagerHelper = new \Magento\Framework\TestFramework\Unit\Helper\ObjectManager($this);
+
         $columnMock = $this
             ->getMockBuilder('Magento\Backend\Block\Widget\Grid\Column')
             ->disableOriginalConstructor()
             ->getMock();
 
-        $objectManagerHelper = new \Magento\Framework\TestFramework\Unit\Helper\ObjectManager($this);
+        $contextMock = $this
+            ->getMockBuilder('\Magento\Backend\Block\Context')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $serializerMock = $this->getMockBuilder(\Magento\Framework\Serialize\Serializer\Json::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['serialize'])
+            ->getMock();
+
+        $loggerMock = $this->getMockBuilder(\Ebizmarts\SagePaySuite\Model\Logger\Logger::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $additionalInformation = $objectManagerHelper
+            ->getObject(\Ebizmarts\SagePaySuite\Helper\AdditionalInformation::class,
+                [
+                    'serializer' => $serializerMock,
+                    'logger' => $loggerMock
+                ]
+            );
+
         $this->rulesRendererBlock = $objectManagerHelper->getObject(
             'Ebizmarts\SagePaySuite\Block\Adminhtml\Template\Reports\Fraud\Grid\Renderer\Rules',
-            []
+            [
+                'context' => $contextMock,
+                'information' => $additionalInformation,
+                []
+            ]
         );
 
         $this->rulesRendererBlock->setColumn($columnMock);
@@ -40,7 +67,7 @@ class RulesTest extends \PHPUnit\Framework\TestCase
         $rowMock->expects($this->once())
             ->method('getData')
             ->with('additional_information')
-            ->willReturn([]);
+            ->willReturn("");
 
         $this->assertEquals(
             '',
@@ -57,10 +84,10 @@ class RulesTest extends \PHPUnit\Framework\TestCase
         $rowMock->expects($this->once())
             ->method('getData')
             ->with('additional_information')
-            ->willReturn('a:1:{s:10:"fraudrules";a:0:{}}');
+            ->willReturn('{"fraudrules":"Sage Pay Direct","statusCode":"0000"}');
 
         $this->assertEquals(
-            [],
+            "Sage Pay Direct",
             $this->rulesRendererBlock->render($rowMock)
         );
     }
