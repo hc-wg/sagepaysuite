4a3463f0369132e222c7bd2c1afa4641ddeefc4d
MelissaEbizmarts <melissa@ebizmarts.com>
2018-03-09 12:51:13 -0300

    Rename variables

--- Ui/Component/Listing/Column/Fraud.php
+++ Ui/Component/Listing/Column/Fraud.php
@@ -2,83 +2,49 @@
 
 namespace Ebizmarts\SagePaySuite\Ui\Component\Listing\Column;
 
-use Ebizmarts\SagePaySuite\Model\Logger\Logger;
+use Ebizmarts\SagePaySuite\Helper\Data;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\View\Asset\Repository;
 use \Magento\Sales\Api\OrderRepositoryInterface;
 use \Magento\Framework\View\Element\UiComponent\ContextInterface;
 use \Magento\Framework\View\Element\UiComponentFactory;
 use \Magento\Ui\Component\Listing\Columns\Column;
-use \Magento\Framework\Api\SearchCriteriaBuilder;
-use \Magento\Store\Model\StoreManagerInterface;
 
 class Fraud extends Column
 {
     /**
      * @var OrderRepositoryInterface
      */
-    protected $_orderRepository;
-    /**
-     * @var SearchCriteriaBuilder
-     */
-    protected $_searchCriteria;
-    /**
-     * @var \Magento\Framework\View\Asset\Repository
-     */
-    protected $_assetRepository;
-    /**
-     * @var \Magento\Framework\App\RequestInterface
-     */
-    protected $_requestInterfase;
-    /**
-     * @var \Ebizmarts\SagePaySuite\Helper\Data
-     */
-    protected $_helper;
+    private $orderRepository;
 
     /**
-     * Logging instance
-     * @var \Ebizmarts\SagePaySuite\Model\Logger\Logger
+     * @var Repository
      */
-    private $suiteLogger;
-
-    private $_logger;
-
+    private $assetRepository;
     /**
-     * Monkey constructor.
-     * @param ContextInterface $context
-     * @param UiComponentFactory $uiComponentFactory
-     * @param OrderRepositoryInterface $orderRepository
-     * @param \Magento\Framework\View\Asset\Repository $assetRepository
-     * @param \Magento\Framework\App\RequestInterface $requestInterface
-     * @param SearchCriteriaBuilder $criteria
-     * @param \Ebizmarts\SagePaySuite\Helper\Data $helper
-     *
-
-     * @param array $components
-     * @param array $data
-     * @param Logger $suiteLogger
-     * @param \Psr\Log\LoggerInterface $logger
+     * @var RequestInterface
      */
+    protected $requestInterface;
+    /**
+     * @var Data
+     */
+    private $_helper;
+
     public function __construct(
         ContextInterface $context,
         UiComponentFactory $uiComponentFactory,
         OrderRepositoryInterface $orderRepository,
-        \Magento\Framework\View\Asset\Repository $assetRepository,
-        \Magento\Framework\App\RequestInterface $requestInterface,
-        SearchCriteriaBuilder $criteria,
-         \Ebizmarts\SagePaySuite\Helper\Data $helper,
-        Logger $suiteLogger,
-        \Psr\Log\LoggerInterface $logger,
-
+        Repository $assetRepository,
+        RequestInterface $requestInterface,
+        Data $helper,
         array $components = [],
         array $data = []
     ) {
 
-        $this->_orderRepository = $orderRepository;
-        $this->_searchCriteria  = $criteria;
-        $this->_assetRepository = $assetRepository;
-        $this->_requestInterfase= $requestInterface;
+        $this->orderRepository = $orderRepository;
+        $this->assetRepository = $assetRepository;
+        $this->requestInterface= $requestInterface;
         $this->_helper          = $helper;
-        $this->suiteLogger        = $suiteLogger;
-        $this->_logger          = $logger;
         parent::__construct($context, $uiComponentFactory, $components, $data);
     }
 
@@ -89,56 +55,65 @@ class Fraud extends Column
                 $fieldName = $this->getData('name');
 
                 $orderId = $item['entity_id'];
-                $order= $this->_orderRepository->get($orderId);
-                $additional = $order->getPayment()->getAdditionalInformation();
+                $order= $this->orderRepository->get($orderId);
+                $additional = $order->getPayment();
+                if($additional != null){
+                    $additional = $additional->getAdditionalInformation();
 
-                if(isset($additional['fraudcode']) && isset($additional['fraudrules']) ){
-                    $score = $additional['fraudcode'];
-                    if ($score < 30) {
-                        $params = ['_secure' => $this->_requestInterfase->isSecure()];
-                        $url = $this->_assetRepository->getUrlWithParams('Ebizmarts_SagePaySuite::images/icon-shield-check.png', $params);
-                        $item[$fieldName . '_src'] = $url;
-                    } else if ($score >= 30 && $score <= 49) {
-                        $params = ['_secure' => $this->_requestInterfase->isSecure()];
-                        $url = $this->_assetRepository->getUrlWithParams('Ebizmarts_SagePaySuite::images/icon-shield-zebra.png', $params);
-                        $item[$fieldName . '_src'] = $url;
-                    } else {
-                        $params = ['_secure' => $this->_requestInterfase->isSecure()];
-                        $url = $this->_assetRepository->getUrlWithParams('Ebizmarts_SagePaySuite::images/icon-shield-cross.png', $params);
-                        $item[$fieldName . '_src'] = $url;
+                    if(isset($additional['fraudrules']) && isset($additional['fraudcode'])){
+                        $score = $additional['fraudcode'];
+                        if ($score < 30) {
+                            $params = ['_secure' => $this->requestInterface->isSecure()];
+                            $url = $this->assetRepository->getUrlWithParams('Ebizmarts_SagePaySuite::images/icon-shield-check.png', $params);
+                            $item[$fieldName . '_src'] = $url;
+                        } else if ($score >= 30 && $score <= 49) {
+                            $params = ['_secure' => $this->requestInterface->isSecure()];
+                            $url = $this->assetRepository->getUrlWithParams('Ebizmarts_SagePaySuite::images/icon-shield-zebra.png', $params);
+                            $item[$fieldName . '_src'] = $url;
+                        } else {
+                            $params = ['_secure' => $this->requestInterface->isSecure()];
+                            $url = $this->assetRepository->getUrlWithParams('Ebizmarts_SagePaySuite::images/icon-shield-cross.png', $params);
+                            $item[$fieldName . '_src'] = $url;
+                        }
                     }
-                }
 
-                elseif(isset($additional['fraudcode'])){
-                    $status = $additional['fraudcode'];
-                    switch (strtoupper($status)) {
-                        case 'ACCEPT':
-                            $params = ['_secure' => $this->_requestInterfase->isSecure()];
-                            $url = $this->_assetRepository->getUrlWithParams('Ebizmarts_SagePaySuite::images/icon-shield-check.png', $params);
-                            $item[$fieldName . '_src'] = $url;
-                            break;
-                        case 'DENY':
-                            $params = ['_secure' => $this->_requestInterfase->isSecure()];
-                            $url = $this->_assetRepository->getUrlWithParams('Ebizmarts_SagePaySuite::images/icon-shield-cross.png', $params);
-                            $item[$fieldName . '_src'] = $url;
-                            break;
-                        case 'CHALLENGE':
-                            $params = ['_secure' => $this->_requestInterfase->isSecure()];
-                            $url = $this->_assetRepository->getUrlWithParams('Ebizmarts_SagePaySuite::images/icon-shield-zebra.png', $params);
-                            $item[$fieldName . '_src'] = $url;
-                            break;
-                        case 'NOTCHECKED':
-                            $params = ['_secure' => $this->_requestInterfase->isSecure()];
-                            $url = $this->_assetRepository->getUrlWithParams('Ebizmarts_SagePaySuite::images/icon-shield-outline.png', $params);
-                            $item[$fieldName . '_src'] = $url;
-                            break;
+                    elseif(isset($additional['fraudcode'])){
+                        $status = $additional['fraudcode'];
+                        switch (strtoupper($status)) {
+                            case 'ACCEPT':
+                                $params = ['_secure' => $this->requestInterface->isSecure()];
+                                $url = $this->assetRepository->getUrlWithParams('Ebizmarts_SagePaySuite::images/icon-shield-check.png', $params);
+                                $item[$fieldName . '_src'] = $url;
+                                break;
+                            case 'DENY':
+                                $params = ['_secure' => $this->requestInterface->isSecure()];
+                                $url = $this->assetRepository->getUrlWithParams('Ebizmarts_SagePaySuite::images/icon-shield-cross.png', $params);
+                                $item[$fieldName . '_src'] = $url;
+                                break;
+                            case 'CHALLENGE':
+                                $params = ['_secure' => $this->requestInterface->isSecure()];
+                                $url = $this->assetRepository->getUrlWithParams('Ebizmarts_SagePaySuite::images/icon-shield-zebra.png', $params);
+                                $item[$fieldName . '_src'] = $url;
+                                break;
+                            case 'NOTCHECKED':
+                                $params = ['_secure' => $this->requestInterface->isSecure()];
+                                $url = $this->assetRepository->getUrlWithParams('Ebizmarts_SagePaySuite::images/icon-shield-outline.png', $params);
+                                $item[$fieldName . '_src'] = $url;
+                                break;
+                        }
                     }
+
+
                 }
-
-
             }
+
         }
 
         return $dataSource;
     }
+
+    public function setT3MShields($status){
+
+    }
+
 }
