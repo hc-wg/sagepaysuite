4014e17c714100d7249a1c55c55564f6797d4b68
centerax <pablos.benitez@gmail.com>
2018-03-20 15:03:22 -0300

    fix Count on PHPUnit, not existant on this version

--- Test/Unit/Plugin/ExcludeFilesFromMinificationTest.php
+++ Test/Unit/Plugin/ExcludeFilesFromMinificationTest.php
@@ -54,12 +54,12 @@ class ExcludeFilesFromMinificationTest extends \PHPUnit_Framework_TestCase
         $pluginConfig = $xml->xpath('/config/type[@name="Magento\Framework\View\Asset\Minification"]');
 
         $this->assertNotEmpty($pluginConfig);
-        $this->assertCount(1, $pluginConfig);
+        $this->assertEquals(1, count($pluginConfig));
 
         $pluginNode = $pluginConfig[0];
 
         $pluginNodeAttributes = $pluginNode->plugin->attributes();
-        $this->assertCount(2, $pluginNodeAttributes);
+        $this->assertEquals(2, count($pluginNodeAttributes));
 
         $this->assertEquals('preventPiRemoteJsMinification', $pluginNodeAttributes['name']);
         $this->assertEquals('Ebizmarts\SagePaySuite\Plugin\ExcludeFilesFromMinification', $pluginNodeAttributes['type']);
