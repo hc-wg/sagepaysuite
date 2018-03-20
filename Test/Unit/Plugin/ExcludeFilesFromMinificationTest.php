e699f8447d84389ddcb02d786cc8f784580fb9b4
centerax <pablos.benitez@gmail.com>
2018-03-20 11:34:14 -0300

    more tests for plugin

--- Test/Unit/Plugin/ExcludeFilesFromMinificationTest.php
+++ Test/Unit/Plugin/ExcludeFilesFromMinificationTest.php
@@ -3,13 +3,35 @@ declare(strict_types=1);
 
 namespace Ebizmarts\SagePaySuite\Test\Unit\Plugin;
 
+use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
+use Magento\Framework\View\Asset\Minification;
 
 class ExcludeFilesFromMinificationTest extends \PHPUnit\Framework\TestCase
 {
 
-    public function testPlugin()
+    /**
+     * @dataProvider pluginDataprovider
+     */
+    public function testPluginJsEmptyResult(array $result, string $contentType, array $expected)
     {
+        $minificationMock = $this->createMock(Minification::class);
+        $objectManager = new ObjectManager($this);
 
+        /** @var \Ebizmarts\SagePaySuite\Plugin\ExcludeFilesFromMinification $excludesPlugin */
+        $excludesPlugin = $objectManager->getObject(\Ebizmarts\SagePaySuite\Plugin\ExcludeFilesFromMinification::class);
+
+        $pluginResult = $excludesPlugin->afterGetExcludes($minificationMock, $result, $contentType);
+
+        $this->assertEquals($expected, $pluginResult);
+    }
+
+    public function pluginDataprovider() : array
+    {
+        return [
+            [[], 'js', ['api/v1/js/sagepay']],
+            [[], 'css', []],
+            [['/tiny_mce/'], 'js', ['/tiny_mce/', 'api/v1/js/sagepay']],
+        ];
     }
 
     public function testDiXmlDirectiveExists()
