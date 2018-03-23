8934612499f2c34b2322cecd685ec54b4c937e77
centerax <pablos.benitez@gmail.com>
2018-03-23 10:30:36 -0300

    Fix case when statusdetail does not have colon. closes #341

--- Test/Unit/Controller/Form/FailureTest.php
+++ Test/Unit/Controller/Form/FailureTest.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 /**
  * Copyright © 2015 ebizmarts. All rights reserved.
  * See LICENSE.txt for license details.
@@ -10,98 +11,31 @@ use Magento\Framework\TestFramework\Unit\Helper\ObjectManager as ObjectManagerHe
 
 class FailureTest extends \PHPUnit\Framework\TestCase
 {
-    public function testExecute()
+    /**
+     * @dataProvider responseStatusDataProvider
+     */
+    public function testExecute($responseData, $expectedErrorMessage)
     {
-        $responseMock = $this
-            ->getMockBuilder('Magento\Framework\App\Response\Http', [], [], '', false)
-            ->disableOriginalConstructor()
-            ->getMock();
+        $responseMock = $this->makeResponseMock();
 
-        $requestMock = $this
-            ->getMockBuilder('Magento\Framework\HTTP\PhpEnvironment\Request')
-            ->disableOriginalConstructor()
-            ->getMock();
+        $requestMock = $this->makeRequestMock();
 
-        $redirectMock = $this->getMockForAbstractClass('Magento\Framework\App\Response\RedirectInterface');
+        $redirectMock = $this->makeRedirectMock();
 
-        $messageManagerMock = $this->getMockBuilder('Magento\Framework\Message\ManagerInterface')
-            ->disableOriginalConstructor()
-            ->getMock();
+        $messageManagerMock = $this->makeMessageManagerMock();
 
-        $contextMock = $this->getMockBuilder('Magento\Framework\App\Action\Context')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $contextMock->expects($this->any())
-            ->method('getRequest')
-            ->will($this->returnValue($requestMock));
-        $contextMock->expects($this->any())
-            ->method('getResponse')
-            ->will($this->returnValue($responseMock));
-        $contextMock->expects($this->any())
-            ->method('getRedirect')
-            ->will($this->returnValue($redirectMock));
-        $contextMock->expects($this->any())
-            ->method('getMessageManager')
-            ->will($this->returnValue($messageManagerMock));
+        $contextMock = $this->makeContextMock($requestMock, $responseMock, $redirectMock, $messageManagerMock);
 
-        $formModelMock = $this
-            ->getMockBuilder('Ebizmarts\SagePaySuite\Model\Form')
-            ->disableOriginalConstructor()
-            ->getMock();
+        $formModelMock = $this->makeFormModelMock();
         $formModelMock->expects($this->any())
             ->method('decodeSagePayResponse')
-            ->will($this->returnValue([
-                "Status" => "REJECTED",
-                "StatusDetail" => "2000 : Invalid Card"
-            ]));
+            ->will($this->returnValue($responseData));
 
-        $quoteMock = $this->getMockBuilder('\Magento\Quote\Model\Quote')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $quoteMock
-            ->expects($this->once())
-            ->method('load')
-            ->willReturnSelf();
-        $quoteMock
-            ->expects($this->once())
-            ->method('getId')
-            ->willReturn(1234);
-        $quoteMock
-            ->expects($this->once())
-            ->method('setIsActive')
-            ->with(1);
-        $quoteMock
-            ->expects($this->once())
-            ->method('setReservedOrderId')
-            ->with(null);
-        $quoteMock
-            ->expects($this->once())
-            ->method('save');
+        $quoteMock = $this->makeQuoteMock();
 
-        $quoteFactoryMock = $this->getMockBuilder('\Magento\Quote\Model\QuoteFactory')
-            ->disableOriginalConstructor()
-            ->setMethods(["create"])
-            ->getMock();
-        $quoteFactoryMock->expects($this->once())
-            ->method('create')
-            ->willReturn($quoteMock);
+        $quoteFactoryMock = $this->makeQuoteFactoryMock($quoteMock);
 
-        $orderMock = $this->getMockBuilder(\Magento\Sales\Model\Order::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-        $orderMock->expects($this->once())
-            ->method('loadByIncrementId')
-            ->willReturnSelf();
-        $orderMock->expects($this->once())
-            ->method('cancel')
-            ->willReturnSelf();
-        $orderFactoryMock = $this->getMockBuilder(\Magento\Sales\Model\OrderFactory::class)
-            ->disableOriginalConstructor()
-            ->setMethods(["create"])
-            ->getMock();
-        $orderFactoryMock->expects($this->once())
-            ->method('create')
-            ->willReturn($orderMock);
+        $orderFactoryMock = $this->makeOrderMock();
 
         $objectManagerHelper = new ObjectManagerHelper($this);
         $formFailureController = $objectManagerHelper->getObject(
@@ -117,7 +51,7 @@ class FailureTest extends \PHPUnit\Framework\TestCase
 
         $messageManagerMock->expects($this->once())
             ->method('addError')
-            ->with("REJECTED: Invalid Card");
+            ->with($expectedErrorMessage);
 
         $redirectMock
             ->expects($this->once())
@@ -127,44 +61,37 @@ class FailureTest extends \PHPUnit\Framework\TestCase
         $formFailureController->execute();
     }
 
+    public function responseStatusDataProvider()
+    {
+        return [
+            [["Status" => "REJECTED", "StatusDetail" => "2000 : Invalid Card"], 'REJECTED: Invalid Card'],
+            [
+                [
+                    "VendorTxCode" => "2000000220-2018-11-12",
+                    "VPSTxId" => "{6B8UXLCZ-32C7-99DB-671F-90E4B13282EB}",
+                    "Status" => "REJECTED",
+                    "StatusDetail" => "The number of authorisation attempts exceeds the limit.",
+                    "GiftAid" => "0",
+                    "Amount" => "20.18"
+                ],
+                'REJECTED: The number of authorisation attempts exceeds the limit.'
+            ],
+        ];
+    }
+
     public function testExecuteException()
     {
-        $responseMock = $this
-            ->getMockBuilder('Magento\Framework\App\Response\Http', [], [], '', false)
-            ->disableOriginalConstructor()
-            ->getMock();
+        $responseMock = $this->makeResponseMock();
 
-        $requestMock = $this
-            ->getMockBuilder('Magento\Framework\HTTP\PhpEnvironment\Request')
-            ->disableOriginalConstructor()
-            ->getMock();
+        $requestMock = $this->makeRequestMock();
 
-        $redirectMock = $this->getMockForAbstractClass('Magento\Framework\App\Response\RedirectInterface');
+        $redirectMock = $this->makeRedirectMock();
 
-        $messageManagerMock = $this->getMockBuilder('Magento\Framework\Message\ManagerInterface')
-            ->disableOriginalConstructor()
-            ->getMock();
+        $messageManagerMock = $this->makeMessageManagerMock();
 
-        $contextMock = $this->getMockBuilder('Magento\Framework\App\Action\Context')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $contextMock->expects($this->any())
-            ->method('getRequest')
-            ->will($this->returnValue($requestMock));
-        $contextMock->expects($this->any())
-            ->method('getResponse')
-            ->will($this->returnValue($responseMock));
-        $contextMock->expects($this->any())
-            ->method('getRedirect')
-            ->will($this->returnValue($redirectMock));
-        $contextMock->expects($this->any())
-            ->method('getMessageManager')
-            ->will($this->returnValue($messageManagerMock));
+        $contextMock = $this->makeContextMock($requestMock, $responseMock, $redirectMock, $messageManagerMock);
 
-        $formModelMock = $this
-            ->getMockBuilder('Ebizmarts\SagePaySuite\Model\Form')
-            ->disableOriginalConstructor()
-            ->getMock();
+        $formModelMock = $this->makeFormModelMock();
         $formModelMock->expects($this->any())
             ->method('decodeSagePayResponse')
             ->willReturn([]);
@@ -196,42 +123,17 @@ class FailureTest extends \PHPUnit\Framework\TestCase
 
     public function testExecuteOrderCancelException()
     {
-        $responseMock = $this
-            ->getMockBuilder('Magento\Framework\App\Response\Http', [], [], '', false)
-            ->disableOriginalConstructor()
-            ->getMock();
+        $responseMock = $this->makeResponseMock();
 
-        $requestMock = $this
-            ->getMockBuilder('Magento\Framework\HTTP\PhpEnvironment\Request')
-            ->disableOriginalConstructor()
-            ->getMock();
+        $requestMock = $this->makeRequestMock();
 
-        $redirectMock = $this->getMockForAbstractClass('Magento\Framework\App\Response\RedirectInterface');
+        $redirectMock = $this->makeRedirectMock();
 
-        $messageManagerMock = $this->getMockBuilder('Magento\Framework\Message\ManagerInterface')
-            ->disableOriginalConstructor()
-            ->getMock();
+        $messageManagerMock = $this->makeMessageManagerMock();
 
-        $contextMock = $this->getMockBuilder('Magento\Framework\App\Action\Context')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $contextMock->expects($this->any())
-            ->method('getRequest')
-            ->will($this->returnValue($requestMock));
-        $contextMock->expects($this->any())
-            ->method('getResponse')
-            ->will($this->returnValue($responseMock));
-        $contextMock->expects($this->any())
-            ->method('getRedirect')
-            ->will($this->returnValue($redirectMock));
-        $contextMock->expects($this->any())
-            ->method('getMessageManager')
-            ->will($this->returnValue($messageManagerMock));
+        $contextMock = $this->makeContextMock($requestMock, $responseMock, $redirectMock, $messageManagerMock);
 
-        $formModelMock = $this
-            ->getMockBuilder('Ebizmarts\SagePaySuite\Model\Form')
-            ->disableOriginalConstructor()
-            ->getMock();
+        $formModelMock = $this->makeFormModelMock();
         $formModelMock->expects($this->any())
             ->method('decodeSagePayResponse')
             ->will($this->returnValue([
@@ -259,13 +161,7 @@ class FailureTest extends \PHPUnit\Framework\TestCase
             ->expects($this->never())
             ->method('save');
 
-        $quoteFactoryMock = $this->getMockBuilder('\Magento\Quote\Model\QuoteFactory')
-            ->disableOriginalConstructor()
-            ->setMethods(["create"])
-            ->getMock();
-        $quoteFactoryMock->expects($this->once())
-            ->method('create')
-            ->willReturn($quoteMock);
+        $quoteFactoryMock = $this->makeQuoteFactoryMock($quoteMock);
 
         $orderMock = $this->getMockBuilder(\Magento\Sales\Model\Order::class)
             ->disableOriginalConstructor()
@@ -273,9 +169,11 @@ class FailureTest extends \PHPUnit\Framework\TestCase
         $orderMock->expects($this->once())
             ->method('loadByIncrementId')
             ->willReturnSelf();
+
+        $message = new \Magento\Framework\Phrase("Can not cancel order.");
         $orderMock->expects($this->once())
             ->method('cancel')
-            ->willThrowException(new \Exception(new \Magento\Framework\Phrase("Can not cancel order.")));
+            ->willThrowException(new \Exception($message->__toString()));
         $orderFactoryMock = $this->getMockBuilder(\Magento\Sales\Model\OrderFactory::class)
             ->disableOriginalConstructor()
             ->setMethods(["create"])
@@ -306,5 +204,119 @@ class FailureTest extends \PHPUnit\Framework\TestCase
             ->with($this->anything(), "checkout/cart", []);
 
         $formFailureController->execute();
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    private function makeResponseMock(): \PHPUnit_Framework_MockObject_MockObject
+    {
+        $responseMock = $this->getMockBuilder('Magento\Framework\App\Response\Http', [], [], '',
+                false)->disableOriginalConstructor()->getMock();
+
+        return $responseMock;
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    private function makeRequestMock(): \PHPUnit_Framework_MockObject_MockObject
+    {
+        $requestMock = $this->getMockBuilder('Magento\Framework\HTTP\PhpEnvironment\Request')->disableOriginalConstructor()->getMock();
+
+        return $requestMock;
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    private function makeMessageManagerMock(): \PHPUnit_Framework_MockObject_MockObject
+    {
+        $messageManagerMock = $this->getMockBuilder('Magento\Framework\Message\ManagerInterface')->disableOriginalConstructor()->getMock();
+
+        return $messageManagerMock;
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    private function makeRedirectMock(): \PHPUnit_Framework_MockObject_MockObject
+    {
+        $redirectMock = $this->getMockForAbstractClass('Magento\Framework\App\Response\RedirectInterface');
+
+        return $redirectMock;
+    }
+
+    /**
+     * @param $requestMock
+     * @param $responseMock
+     * @param $redirectMock
+     * @param $messageManagerMock
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    private function makeContextMock(
+        $requestMock,
+        $responseMock,
+        $redirectMock,
+        $messageManagerMock
+    ): \PHPUnit_Framework_MockObject_MockObject {
+        $contextMock = $this->getMockBuilder('Magento\Framework\App\Action\Context')->disableOriginalConstructor()->getMock();
+        $contextMock->expects($this->any())->method('getRequest')->will($this->returnValue($requestMock));
+        $contextMock->expects($this->any())->method('getResponse')->will($this->returnValue($responseMock));
+        $contextMock->expects($this->any())->method('getRedirect')->will($this->returnValue($redirectMock));
+        $contextMock->expects($this->any())->method('getMessageManager')->will($this->returnValue($messageManagerMock));
+
+        return $contextMock;
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    private function makeFormModelMock(): \PHPUnit_Framework_MockObject_MockObject
+    {
+        $formModelMock = $this->getMockBuilder('Ebizmarts\SagePaySuite\Model\Form')->disableOriginalConstructor()->getMock();
+
+        return $formModelMock;
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    private function makeQuoteMock(): \PHPUnit_Framework_MockObject_MockObject
+    {
+        $quoteMock = $this->getMockBuilder('\Magento\Quote\Model\Quote')->disableOriginalConstructor()->getMock();
+        $quoteMock->expects($this->once())->method('load')->willReturnSelf();
+        $quoteMock->expects($this->once())->method('getId')->willReturn(1234);
+        $quoteMock->expects($this->once())->method('setIsActive')->with(1);
+        $quoteMock->expects($this->once())->method('setReservedOrderId')->with(null);
+        $quoteMock->expects($this->once())->method('save');
+
+        return $quoteMock;
+    }
+
+    /**
+     * @param $quoteMock
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    private function makeQuoteFactoryMock($quoteMock): \PHPUnit_Framework_MockObject_MockObject
+    {
+        $quoteFactoryMock = $this->getMockBuilder('\Magento\Quote\Model\QuoteFactory')->disableOriginalConstructor()->setMethods(["create"])->getMock();
+        $quoteFactoryMock->expects($this->once())->method('create')->willReturn($quoteMock);
+
+        return $quoteFactoryMock;
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    private function makeOrderMock(): \PHPUnit_Framework_MockObject_MockObject
+    {
+        $orderMock = $this->getMockBuilder(\Magento\Sales\Model\Order::class)->disableOriginalConstructor()->getMock();
+        $orderMock->expects($this->once())->method('loadByIncrementId')->willReturnSelf();
+        $orderMock->expects($this->once())->method('cancel')->willReturnSelf();
+        $orderFactoryMock = $this->getMockBuilder(\Magento\Sales\Model\OrderFactory::class)->disableOriginalConstructor()->setMethods(["create"])->getMock();
+        $orderFactoryMock->expects($this->once())->method('create')->willReturn($orderMock);
+
+        return $orderFactoryMock;
     }
 }
