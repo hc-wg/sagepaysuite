75f4e7a83eea979d4fb75251ff6f127ba1d00bd5
centerax <pablos.benitez@gmail.com>
2018-07-24 16:40:50 -0300

    Test placeholder for SystemConfigEdit

---
+++ Test/Unit/Observer/SystemConfigEditTest.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright © 2018 ebizmarts. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+namespace Ebizmarts\SagePaySuite\Test\Unit\Observer;
+
+
+class SystemConfigEditTest extends \PHPUnit\Framework\TestCase
+{
+
+}
