f9739561a25c7c9849f93f4c71ea037037f6afb9
Kevinrx <kevin@ebizmarts.com>
2018-07-27 19:21:27 -0300

    Fix token breaking checkout. closes #423

--- view/frontend/web/js/view/payment/method-renderer/server-method.js
+++ view/frontend/web/js/view/payment/method-renderer/server-method.js
@@ -81,12 +81,13 @@ define(
                  if (window.checkoutConfig.payment.ebizmarts_sagepaysuiteserver) {
                     if (window.checkoutConfig.payment.ebizmarts_sagepaysuiteserver.token_enabled &&
                         window.checkoutConfig.payment.ebizmarts_sagepaysuiteserver.token_enabled == true) {
-                        if(document.getElementById("remembertoken").checked == true){
-                            use_token : true;
+                        if(document.getElementById('remembertoken') &&
+                            document.getElementById('remembertoken').checked == true){
+                            use_token : false;
                             save_token : true;
                         }
                         else {
-                            self.use_token = false;
+                            self.use_token = true;
                             self.save_token =  false;
                         }
                     }
