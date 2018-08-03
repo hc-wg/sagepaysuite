72bce436f78903dfea8dc235f6e557201fc2911a
Bettina Cerban <bettinacerban@gmail.com>
2018-08-03 16:01:40 -0300

    add failing test for current case

--- Test/Unit/Model/PiRequestManagement/TransactionAmountDefaultTest.php
+++ Test/Unit/Model/PiRequestManagement/TransactionAmountDefaultTest.php
@@ -5,9 +5,6 @@ namespace Ebizmarts\SagePaySuite\Test\Unit\Model\PiRequestManagement;
 
 use Ebizmarts\SagePaySuite\Model\PiRequestManagement\TransactionAmountDefaultPi;
 use Ebizmarts\SagePaySuite\Model\PiRequestManagement\TransactionAmountDefaultPost;
-use Ebizmarts\SagePaySuite\Model\PiRequestManagement\TransactionAmountJapaneseYen;
-use Ebizmarts\SagePaySuite\Model\PiRequestManagement\TransactionAmountPost;
-use Ebizmarts\SagePaySuite\Model\PiRequestManagement\TransactionAmountPostJapaneseYen;
 
 class TransactionAmountDefaultTest extends \PHPUnit\Framework\TestCase
 {
@@ -37,7 +34,8 @@ class TransactionAmountDefaultTest extends \PHPUnit\Framework\TestCase
         return [
             [1, 1],
             [9539.84, 9539.84],
-            [6469.20, 6469.20]
+            [6469.20, 6469.20],
+            [146.20, 146.20]
         ];
     }
 
