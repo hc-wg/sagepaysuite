07c5e704dec9cd87c1ff018667775d81c886813d
Bettina Cerban <bettinacerban@gmail.com>
2018-08-07 17:21:19 -0300

    prevent form rendering if t&c is not selected

--- view/frontend/web/js/view/payment/method-renderer/server-method.js
+++ view/frontend/web/js/view/payment/method-renderer/server-method.js
@@ -159,7 +159,11 @@ define(
                             }
                         ).fail(
                             function (response) {
-                                self.showPaymentError("Unable to save payment method.");
+                                if (response.responseJSON) {
+                                    self.showPaymentError(response.responseJSON.message);
+                                } else {
+                                    self.showPaymentError("Unable to save payment info.");
+                                }
                             }
                         );
                     }
