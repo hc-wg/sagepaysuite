393fd0b9dc798096ddc74f911206f3898c6740ac
Kevinrx <kevin@ebizmarts.com>
2019-04-10 17:50:21 -0300

    Add param to calculateRefundAmount

--- Model/PI.php
+++ Model/PI.php
@@ -224,7 +224,7 @@ class PI extends \Magento\Payment\Model\Method\Cc
             $refundAmount = $baseAmount * 100;
 
             if ($this->config->getCurrencyConfig() !== CONFIG::CURRENCY_BASE) {
-                $refundAmount = $this->calculateRefundAmount($baseAmount, $order);
+                $refundAmount = $this->calculateRefundAmount($baseAmount, $order, $refundAmount);
             }
 
             /** @var \Ebizmarts\SagePaySuite\Api\SagePayData\PiTransactionResultInterface $refundResult */
@@ -400,7 +400,7 @@ class PI extends \Magento\Payment\Model\Method\Cc
      * @param Order $order
      * @return float|int
      */
-    public function calculateRefundAmount($baseAmount, Order $order)
+    public function calculateRefundAmount($baseAmount, Order $order, $refundAmount)
     {
         $orderCurrencyCode = $order->getOrderCurrencyCode();
         $baseCurrencyCode = $order->getBaseCurrencyCode();
