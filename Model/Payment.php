4355e1dd4e29f1b0b0a9e02d79e5e9f0f82f2b97
Kevinrx <kevin@ebizmarts.com>
2019-04-10 16:42:24 -0300

    Typo mistake fixed

--- Model/Payment.php
+++ Model/Payment.php
@@ -54,7 +54,7 @@ class Payment
                 $result = [];
                 if ($this->isDeferredOrRepeatDeferredAction($paymentAction)) {
                     $action = 'releasing';
-                    if ($this->config->getCurrencyConfig() != CONFIG::CURRENCY_BASE) {
+                    if ($this->config->getCurrencyConfig() !== CONFIG::CURRENCY_BASE) {
                         $amount = $this->calculateAmount($amount, $order);
                     }
                     $result = $this->sharedApi->captureDeferredTransaction($transactionId, $amount, $order);
