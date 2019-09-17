ffd119d092dce978bfcf06593f7b8d6692744fff
centerax <pablos.benitez@gmail.com>
2019-09-17 11:58:54 -0300

    update tests for moto pi

--- Test/Unit/Controller/Adminhtml/PI/RequestTest.php
+++ Test/Unit/Controller/Adminhtml/PI/RequestTest.php
@@ -360,41 +360,7 @@ class RequestTest extends \PHPUnit\Framework\TestCase
             ]
         );
 
-        $this->requestMock->expects($this->any())->method('getPost')
-            ->withConsecutive(
-                ['merchant_session_key'],
-                ['card_identifier'],
-                ['card_exp_month'],
-                ['card_exp_year'],
-                ['card_last4'],
-                ['card_type'],
-                ['javascript_enabled'],
-                ['accept_headers'],
-                ['language'],
-                ['user_agent'],
-                ['java_enabled'],
-                ['color_depth'],
-                ['screen_width'],
-                ['screen_height'],
-                ['timezone']
-            )
-            ->willReturnOnConsecutiveCalls(
-            '1B436F04-E6B5-4785-A5B9-B28E9DDC1B92',
-            '74D926FD-5E87-451A-A089-7F6B4D1A76C9',
-            '',
-            '',
-            '',
-            '',
-            1,
-            '*/*',
-            'en-US',
-            'Mozilla\/5.0',
-            0,
-            24,
-            1024,
-            768,
-            180
-        );
+        $this->setRequestAsserts();
 
         $requesterMock = $this->getMockBuilder(\Ebizmarts\SagePaySuite\Model\PiRequestManagement\MotoManagement::class)
             ->disableOriginalConstructor()
@@ -434,6 +400,8 @@ class RequestTest extends \PHPUnit\Framework\TestCase
             new \Magento\Framework\Exception\LocalizedException(new \Magento\Framework\Phrase("INVALID"))
         );
 
+        $this->setRequestAsserts();
+
         $requesterMock = $this->getMockBuilder(\Ebizmarts\SagePaySuite\Model\PiRequestManagement\MotoManagement::class)
             ->disableOriginalConstructor()
             ->getMock();
@@ -472,5 +440,16 @@ class RequestTest extends \PHPUnit\Framework\TestCase
             ->expects($this->once())
             ->method('setData')
             ->with($result);
+    }
+
+    private function setRequestAsserts(): void
+    {
+        $this->requestMock->expects($this->any())->method('getPost')->withConsecutive(['merchant_session_key'],
+                ['card_identifier'], ['card_exp_month'], ['card_exp_year'], ['card_last4'], ['card_type'],
+                ['javascript_enabled'], ['accept_headers'], ['language'], ['user_agent'], ['java_enabled'],
+                ['color_depth'], ['screen_width'], ['screen_height'],
+                ['timezone'])->willReturnOnConsecutiveCalls('1B436F04-E6B5-4785-A5B9-B28E9DDC1B92',
+                '74D926FD-5E87-451A-A089-7F6B4D1A76C9', '', '', '', '', 1, '*/*', 'en-US', 'Mozilla\/5.0', 0, 24, 1024,
+                768, 180);
     }
 }
