2c846c0495190d2804e80869a7bd7f3e0e6e9a21
Kevin Rinaldi <kevin@ebizmarts.com>
2019-11-14 16:46:20 -0300

    Update construct notes

--- Controller/PI/Callback3D.php
+++ Controller/PI/Callback3D.php
@@ -48,6 +48,7 @@ class Callback3D extends Action implements CsrfAwareActionInterface
      * @param PiRequestManagerFactory $piReqManagerFactory
      * @param OrderRepositoryInterface $orderRepository
      * @param CryptAndCodeData $cryptAndCode
+     * @param RecoverCartAndCancelOrder $recoverCartAndCancelOrder
      */
     public function __construct(
         Context $context,
