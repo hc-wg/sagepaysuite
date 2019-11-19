7ca1692c8d65f319e6f6e1f77d9face31047e0fb
Juan-ebizmarts <juanandres@ebizmarts.com>
2019-11-19 16:07:42 -0300

    Requested changes

--- Ui/Component/Listing/Column/PostCodeCheck.php
+++ Ui/Component/Listing/Column/PostCodeCheck.php
@@ -1,80 +1,40 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: juan
- * Date: 2019-11-08
- * Time: 16:13
+ * Copyright © 2017 ebizmarts. All rights reserved.
+ * See LICENSE.txt for license details.
  */
 
 namespace Ebizmarts\SagePaySuite\Ui\Component\Listing\Column;
 
-use Magento\Framework\App\RequestInterface;
-use Magento\Framework\Exception\InputException;
-use Magento\Framework\Exception\NoSuchEntityException;
-use Magento\Framework\View\Asset\Repository;
-use \Magento\Sales\Api\OrderRepositoryInterface;
 use \Magento\Framework\View\Element\UiComponent\ContextInterface;
 use \Magento\Framework\View\Element\UiComponentFactory;
 use \Magento\Ui\Component\Listing\Columns\Column;
-use Ebizmarts\SagePaySuite\Model\Logger\Logger;
-use \Ebizmarts\SagePaySuite\Helper\AdditionalInformation;
+use \Ebizmarts\SagePaySuite\Model\OrderGridInfo;
 
 class PostCodeCheck extends Column
 {
-    const IMAGE_PATH = 'Ebizmarts_SagePaySuite::images/icon-shield-';
 
     /**
-     * @var OrderRepositoryInterface
+     * @var \Ebizmarts\SagePaySuite\Model\OrderGridInfo
      */
-    private $orderRepository;
-    /**
-     * @var Repository
-     */
-    private $assetRepository;
-    /**
-     * @var RequestInterface
-     */
-    protected $requestInterface;
-
-    /**
-     * Logging instance
-     * @var \Ebizmarts\SagePaySuite\Model\Logger\Logger
-     */
-    private $suiteLogger;
-
-    /**
-     * @var AdditionalInformation
-     */
-    private $serialize;
+    private $orderGridInfo;
 
     /**
      * PostCodeCheck constructor.
-     * @param Logger $suiteLogger
+     * @param OrderGridInfo $orderGridInfo
      * @param ContextInterface $context
      * @param UiComponentFactory $uiComponentFactory
-     * @param OrderRepositoryInterface $orderRepository
-     * @param Repository $assetRepository
-     * @param RequestInterface $requestInterface
-     * @param AdditionalInformation $serialize
      * @param array $components
      * @param array $data
      */
     public function __construct(
-        Logger $suiteLogger,
+        OrderGridInfo $orderGridInfo,
         ContextInterface $context,
         UiComponentFactory $uiComponentFactory,
-        OrderRepositoryInterface $orderRepository,
-        Repository $assetRepository,
-        RequestInterface $requestInterface,
-        AdditionalInformation $serialize,
         array $components = [],
         array $data = []
     ) {
-        $this->suiteLogger      = $suiteLogger;
-        $this->orderRepository  = $orderRepository;
-        $this->assetRepository  = $assetRepository;
-        $this->requestInterface = $requestInterface;
-        $this->serialize        = $serialize;
+        $this->orderGridInfo = $orderGridInfo;
         parent::__construct($context, $uiComponentFactory, $components, $data);
     }
 
@@ -84,71 +44,8 @@ class PostCodeCheck extends Column
      */
     public function prepareDataSource(array $dataSource)
     {
-        if (isset($dataSource['data']['items'])) {
-            foreach ($dataSource['data']['items'] as &$item) {
-                if (strpos($item['payment_method'], "sagepaysuite") !== false) {
-                    $fieldName = $this->getFieldName();
-                    $orderId = $item['entity_id'];
-                    $params = ['_secure' => $this->requestInterface->isSecure()];
-                    try {
-                        $order = $this->orderRepository->get($orderId);
-                    } catch (InputException $e) {
-                        $this->suiteLogger->logException($e, [__METHOD__, __LINE__]);
-                        continue;
-                    } catch (NoSuchEntityException $e) {
-                        $this->suiteLogger->logException($e, [__METHOD__, __LINE__]);
-                        continue;
-                    }
-                    $payment = $order->getPayment();
-
-                    if ($payment !== null) {
-                        $additional = $payment->getAdditionalInformation();
-                        if (is_string($additional)) {
-                            $additional = $this->serialize->getUnserializedData($additional);
-                        }
-                        if (is_array($additional) && !empty($additional)) {
-                            if (isset($additional["avsCvcCheckPostalCode"])) {
-                                $status = $additional["avsCvcCheckPostalCode"];
-                            } else {
-                                $status = 'NOTPROVIDED';
-                            }
-                            $postcodeResult = $this->getPostCodeResult($status);
-                            $url = $this->assetRepository->getUrlWithParams($postcodeResult, $params);
-                            $item[$fieldName . '_src'] = $url;
-                        }
-                    }
-                }
-            }
-        }
-        return $dataSource;
-    }
-
-    /**
-     * @param $status
-     * @return string
-     */
-    public function getPostCodeResult($status)
-    {
-        $status = strtoupper($status);
-        $postcodeResult = '';
-        switch($status){
-            case 'MATCHED':
-                $postcodeResult = 'check.png';
-                break;
-            case 'NOTCHECKED':
-                $postcodeResult = 'outline.png';
-                break;
-            case 'NOTPROVIDED':
-                $postcodeResult = 'outline.png';
-                break;
-            case 'NOTMATCHED':
-                $postcodeResult = 'cross.png';
-                break;
-            case 'PARTIAL':
-                $postcodeResult = 'zebra.png';
-                break;
-        }
-        return self::IMAGE_PATH . $postcodeResult;
+        $fieldName = $this->getFieldName();
+        return $this->orderGridInfo->prepareColumn($dataSource, "avsCvcCheckPostalCode", $fieldName);
     }
 
     /**
