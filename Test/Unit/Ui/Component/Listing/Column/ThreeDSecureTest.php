e24df6e7dbc160d958b0cbbbdc4436635a3efb4f
Juan-ebizmarts <juanandres@ebizmarts.com>
2019-11-29 16:43:29 -0300

    Remove unused constant

--- Test/Unit/Ui/Component/Listing/Column/ThreeDSecureTest.php
+++ Test/Unit/Ui/Component/Listing/Column/ThreeDSecureTest.php
@@ -65,7 +65,7 @@ class ThreeDSecureTest extends \PHPUnit_Framework_TestCase
                 'items' => [
                     [
                         'entity_id' => self::ENTITY_ID,
-                        'sagepay_threeDSecure_src' => self::IMAGE_URL_CHECK,
+                        self::FIELD_NAME . '_src' => self::IMAGE_URL_CHECK,
                         'payment_method' => "sagepaysuite"
                     ]
                 ]
