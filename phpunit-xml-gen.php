89753149f1da7a14319e5da38c04418cd9587aa4
centerax <pablos.benitez@gmail.com>
2020-01-03 13:57:23 -0300

    split unit tests

---
+++ phpunit-xml-gen.php
@@ -0,0 +1,22 @@
+<?php
+
+$files = array_slice($argv, 1);
+
+$xml = new DOMDocument();
+$xml->load(__DIR__ . '/phpunit.xml');
+
+$testsuite = $xml->createElement('testsuite');
+$testsuite->setAttribute('name', 'partial');
+
+foreach ($files as $file) {
+    $testsuite->appendChild($xml->createElement('file', $file));
+}
+
+$testsuites = $xml->createElement('testsuites');
+$testsuites->appendChild($testsuite);
+$phpunit = $xml->getElementsByTagName('phpunit')->item(0);
+$phpunit->replaceChild($testsuites, $phpunit->getElementsByTagName('testsuite')->item(0));
+
+$xml->save(__DIR__ . '/phpunit-partial.xml');
+
+exit(0);
