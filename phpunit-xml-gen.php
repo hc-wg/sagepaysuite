073ab833120dc23ca591ca4a05986823b769cfba
centerax <pablos.benitez@gmail.com>
2020-01-03 19:13:39 -0300

    fix bug when using api functional

--- phpunit-xml-gen.php
+++ phpunit-xml-gen.php
@@ -14,7 +14,12 @@ foreach ($files as $file) {
 $testsuites = $xml->createElement('testsuites');
 $testsuites->appendChild($testsuite);
 $phpunit = $xml->getElementsByTagName('phpunit')->item(0);
-$phpunit->replaceChild($testsuites, $phpunit->getElementsByTagName('testsuite')->item(0));
+
+try {
+    $phpunit->replaceChild($testsuites, $phpunit->getElementsByTagName('testsuite')->item(0));
+} catch (DOMException $de) {
+    $phpunit->replaceChild($testsuites, $phpunit->getElementsByTagName('testsuites')->item(0));
+}
 
 $xml->save(__DIR__ . '/phpunit-partial.xml');
 
