b0d5b360e1d1c0ff4ed3c2e0d7d1816f6b25aaf2
Kevin Rinaldi <kevin@ebizmarts.com>
2020-02-07 16:05:36 -0300

    Changes in Model/RecoverCartAndCancelOrder.php

--- Model/RecoverCartAndCancelOrder.php
+++ Model/RecoverCartAndCancelOrder.php
@@ -6,9 +6,11 @@
 
 namespace Ebizmarts\SagePaySuite\Model;
 
+use Ebizmarts\SagePaySuite\Model\Session as SagePaySession;
 use Magento\Checkout\Model\Session;
 use Ebizmarts\SagePaySuite\Model\Logger\Logger;
 use Magento\Quote\Model\QuoteFactory;
+use Magento\Sales\Model\Order;
 use Magento\Sales\Model\OrderFactory;
 
 class RecoverCartAndCancelOrder
@@ -39,32 +41,52 @@ class RecoverCartAndCancelOrder
 
     public function execute()
     {
+        $order = $this->getOrder();
+
+        if ($this->verifyIfOrderIsValid($order)) {
+            $quote = $this->checkoutSession->getQuote();
+            if (empty($quote) || empty($quote->getId())) {
+                $order->cancel()->save();
+                $this->recoverQuote($order);
+                $this->removeFlag();
+            }
+        }
+    }
+
+    public function recoverQuote($order)
+    {
+        $quote = $this->quoteFactory->create()->load($order->getQuoteId());
+        if ($quote->getId()) {
+            $quote->setIsActive(1);
+            $quote->setReservedOrderId(null);
+            $quote->save();
+            $this->checkoutSession->replaceQuote($quote);
+        }
+    }
+
+    public function getOrder()
+    {
         /** Reload quote and cancel order if it was pre-saved but not completed */
-        $presavedOrderId = $this->checkoutSession->getData(
-            \Ebizmarts\SagePaySuite\Model\Session::PRESAVED_PENDING_ORDER_KEY
-        );
+        $presavedOrderId = $this->checkoutSession->getData(SagePaySession::PRESAVED_PENDING_ORDER_KEY);
 
         if (!empty($presavedOrderId)) {
             $order = $this->orderFactory->create()->load($presavedOrderId);
-            if ($order !== null && $order->getId() !== null
-                && $order->getState() == \Magento\Sales\Model\Order::STATE_PENDING_PAYMENT
-            ) {
-                $quote = $this->checkoutSession->getQuote();
-                if (empty($quote) || empty($quote->getId())) {
-                    //cancel order
-                    $order->cancel()->save();
-                    //recover quote
-                    $quote = $this->quoteFactory->create()->load($order->getQuoteId());
-                    if ($quote->getId()) {
-                        $quote->setIsActive(1);
-                        $quote->setReservedOrderId(null);
-                        $quote->save();
-                        $this->checkoutSession->replaceQuote($quote);
-                    }
-                    //remove flag
-                    $this->checkoutSession->setData("sagepaysuite_presaved_order_pending_payment", null);
-                }
-            }
+        } else {
+            $order = null;
         }
+
+        return $order;
+    }
+
+    public function verifyIfOrderIsValid($order)
+    {
+        return $order !== null &&
+            $order->getId() !== null &&
+            $order->getState() === Order::STATE_PENDING_PAYMENT;
+    }
+
+    public function removeFlag()
+    {
+        $this->checkoutSession->setData("sagepaysuite_presaved_order_pending_payment", null);
     }
 }
