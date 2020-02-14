2b067b5b299aa7ef460bc29deee72a31f167ada8
Kevin Rinaldi <kevin@ebizmarts.com>
2020-02-14 16:03:22 -0300

    Create Controller to recover cart

---
+++ Controller/Cart/Recover.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Ebizmarts\SagePaySuite\Controller\Cart;
+
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\App\Action\Action;
+use Ebizmarts\SagePaySuite\Model\RecoverCartAndCancelOrder;
+
+class Recover extends Action
+{
+    /** @var RecoverCartAndCancelOrder */
+    private $recoverCartAndCancelOrder;
+
+    /**
+     * Recover constructor.
+     * @param Context $context
+     * @param RecoverCartAndCancelOrder $recoverCartAndCancelOrder
+     */
+    public function __construct(
+        Context $context,
+        RecoverCartAndCancelOrder $recoverCartAndCancelOrder
+    ) {
+        parent::__construct($context);
+        $this->recoverCartAndCancelOrder = $recoverCartAndCancelOrder;
+    }
+
+    public function execute()
+    {
+        $this->recoverCartAndCancelOrder->execute(false);
+        return $this->redirectToCart();
+    }
+
+    public function redirectToCart()
+    {
+        $redirectUrl = 'checkout/cart';
+        return $this->_redirect($redirectUrl);
+    }
+}
