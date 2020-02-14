7d61d49178495e9d9fb87cc3485de7f8d7ae04fa
Kevin Rinaldi <kevin@ebizmarts.com>
2020-02-14 16:04:32 -0300

    Create observer to show a sign if there is a cart to recover

---
+++ Observer/RecoverCart.php
@@ -0,0 +1,76 @@
+<?php
+
+
+namespace Ebizmarts\SagePaySuite\Observer;
+
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Checkout\Model\Session;
+use Magento\Framework\Message\ManagerInterface;
+use Ebizmarts\SagePaySuite\Model\Session as SagePaySession;
+use Ebizmarts\SagePaySuite\Model\Logger\Logger;
+use Magento\Theme\Block\Html\Header\Logo;
+
+class RecoverCart implements ObserverInterface
+{
+    /** @var Session */
+    private $session;
+
+    /** @var Logger */
+    private $suiteLogger;
+
+    /** @var ManagerInterface */
+    private $messageManager;
+
+    /** @var Logo */
+    private $logo;
+
+    public function __construct(
+        Session $session,
+        Logger $suiteLogger,
+        ManagerInterface $messageManager,
+        Logo $logo
+    ) {
+        $this->session = $session;
+        $this->suiteLogger     = $suiteLogger;
+        $this->messageManager  = $messageManager;
+        $this->logo            = $logo;
+    }
+
+    public function execute(Observer $observer)
+    {
+        if ($this->isHomePage($observer)) {
+            $presavedOrderId = $this->session->getData(SagePaySession::PRESAVED_PENDING_ORDER_KEY);
+            $quoteIsActive = $this->session->getData(SagePaySession::QUOTE_IS_ACTIVE);
+            if ($this->checkIfRecoverCartIsPossible($presavedOrderId, $quoteIsActive)) {
+                $message = "There's an order in process, but you can recover the cart ";
+                $message .= sprintf("<a target='_self' href='http://m233.local/sagepaysuite/cart/recover'>%s</a>", __('HERE'));
+                $this->messageManager->addNotice($message);
+            }
+        }
+    }
+
+    /**
+     * @param Observer $observer
+     * @return bool
+     */
+    public function isHomePage()
+    {
+        return $this->logo->isHomePage();
+    }
+
+    public function checkIfRecoverCartIsPossible($presavedOrderId, $quoteIsActive)
+    {
+        return $this->checkPreSavedOrder($presavedOrderId) && $this->checkQuoteIsActive($quoteIsActive);
+    }
+
+    public function checkPreSavedOrder($presavedOrderId)
+    {
+        return !empty($presavedOrderId);
+    }
+
+    public function checkQuoteIsActive($quoteIsActive)
+    {
+        return $quoteIsActive === 0;
+    }
+}
