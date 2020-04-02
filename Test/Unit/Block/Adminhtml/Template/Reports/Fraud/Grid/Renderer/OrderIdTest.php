d5274cb8213d45c360e07ed8d80a404c0e88e5dd
Roberto Sarmiento <roberto@ebizmarts.com>
2020-04-02 14:48:28 -0300

    Recover line removed. Logging errors in case of order is not found. Adding two tests methods

--- Test/Unit/Block/Adminhtml/Template/Reports/Fraud/Grid/Renderer/OrderIdTest.php
+++ Test/Unit/Block/Adminhtml/Template/Reports/Fraud/Grid/Renderer/OrderIdTest.php
@@ -6,9 +6,12 @@
 
 namespace Ebizmarts\SagePaySuite\Test\Unit\Block\Adminhtml\Template\Reports\Fraud\Grid\Renderer;
 
+use Ebizmarts\SagePaySuite\Model\Logger\Logger;
 use Magento\Backend\Block\Context;
 use Magento\Backend\Block\Widget\Grid\Column;
 use Magento\Framework\DataObject;
+use Magento\Framework\Exception\InputException;
+use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
 use Magento\Sales\Model\Order;
 use Magento\Sales\Model\OrderRepository;
@@ -23,37 +26,40 @@ class OrderIdTest extends \PHPUnit\Framework\TestCase
     private $orderIdRendererBlock;
 
     /**
-     * @var OrderRepository
+     * @var OrderRepository | \PHPUnit_Framework_MockObject_MockObject
      */
     private $orderRepositoryMock;
 
     /**
-     * @var Order
+     * @var Order | \PHPUnit_Framework_MockObject_MockObject
      */
     private $orderMock;
 
     /**
-     * @var Context
+     * @var Context | \PHPUnit_Framework_MockObject_MockObject
      */
     private $contextMock;
 
     /**
-     * @var DataObject
+     * @var DataObject | \PHPUnit_Framework_MockObject_MockObject
      */
     private $rowMock;
+
+    /**
+     * Logging instance
+     * @var \Ebizmarts\SagePaySuite\Model\Logger\Logger | \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $suiteLoggerMock;
 
     // @codingStandardsIgnoreStart
     protected function setUp()
     {
-        $urlBuilderMock = $this->getMockBuilder(\Magento\Framework\UrlInterface::class)
+        $this->suiteLoggerMock = $this->getMockBuilder(Logger::class)
             ->disableOriginalConstructor()
             ->getMock();
-        $urlBuilderMock->expects($this->once())->method("getUrl")->with('sales/order/view/', ['order_id' => self::TEST_ORDER_ENTITY_ID])
-            ->willReturn("https://example.comsales/order/view/order_id/1");
 
         //$this->MakeContextMock($urlBuilderMock);
         $this->contextMock = $this->getMockBuilder(Context::class)->disableOriginalConstructor()->setMethods(['getUrlBuilder'])->getMock();
-        $this->contextMock->expects($this->once())->method('getUrlBuilder')->willReturn($urlBuilderMock);
 
         $columnMock = $this->getMockBuilder(Column::class)
             ->disableOriginalConstructor()
@@ -73,9 +79,6 @@ class OrderIdTest extends \PHPUnit\Framework\TestCase
             ->disableOriginalConstructor()
             ->getMock();
 
-        $this->orderMock->expects($this->once())->method('getEntityId')->willReturn(self::TEST_ORDER_ENTITY_ID);
-        $this->orderMock->expects($this->once())->method('getIncrementId')->willReturn(self::TEST_ORDER_INCREMENT_ID);
-
         $this->rowMock = new DataObject(['order_id' => self::TEST_ORDER_ENTITY_ID]);
 
         $objectManagerHelper = new ObjectManager($this);
@@ -83,13 +86,15 @@ class OrderIdTest extends \PHPUnit\Framework\TestCase
             'Ebizmarts\SagePaySuite\Block\Adminhtml\Template\Reports\Fraud\Grid\Renderer\OrderId',
             [
                 'context' => $this->contextMock,
-                'orderRepository' => $this->orderRepositoryMock
+                'orderRepository' => $this->orderRepositoryMock,
+                'suiteLogger' => $this->suiteLoggerMock,
             ]
         );
 
         $this->orderIdRendererBlock->setData('order_id', self::TEST_ORDER_ENTITY_ID);
         $this->orderIdRendererBlock->setColumn($columnMock);
     }
+
     // @codingStandardsIgnoreEnd
 
     public function testRender()
@@ -98,8 +103,51 @@ class OrderIdTest extends \PHPUnit\Framework\TestCase
             ->method('get')->with(self::TEST_ORDER_ENTITY_ID)
             ->willReturn($this->orderMock);
 
+        $urlBuilderMock = $this->getMockBuilder(\Magento\Framework\UrlInterface::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->contextMock->expects($this->once())->method('getUrlBuilder')->willReturn($urlBuilderMock);
+
+        $urlBuilderMock->expects($this->once())->method("getUrl")
+            ->with('sales/order/view/', ['order_id' => self::TEST_ORDER_ENTITY_ID])
+            ->willReturn("https://example.comsales/order/view/order_id/1");
+
+        $this->orderMock->expects($this->once())->method('getEntityId')->willReturn(self::TEST_ORDER_ENTITY_ID);
+        $this->orderMock->expects($this->once())->method('getIncrementId')->willReturn(self::TEST_ORDER_INCREMENT_ID);
+
         $this->assertEquals(
             '<a href="https://example.comsales/order/view/order_id/1">' . self::TEST_ORDER_INCREMENT_ID . '</a>',
+            $this->orderIdRendererBlock
+                ->render(new DataObject(['order_id' => self::TEST_ORDER_ENTITY_ID]))
+        );
+    }
+
+    public function testRenderNoSuchEntityException()
+    {
+        $exceptionMessage = "The entity that was requested doesn't exist. Verify the entity and try again.";
+        $noSuchEntityException = new NoSuchEntityException(__($exceptionMessage));
+
+        $this->orderRepositoryMock->expects($this->once())
+            ->method('get')->with(self::TEST_ORDER_ENTITY_ID)
+            ->willThrowException($noSuchEntityException);
+
+        $this->assertEquals(
+            '',
+            $this->orderIdRendererBlock->render(new DataObject(['order_id' => self::TEST_ORDER_ENTITY_ID]))
+        );
+    }
+
+    public function testRenderInputException()
+    {
+        $exceptionMessage = 'An ID is needed. Set the ID and try again.';
+        $InputException = new InputException(__($exceptionMessage));
+
+        $this->orderRepositoryMock->expects($this->once())
+            ->method('get')->with(self::TEST_ORDER_ENTITY_ID)
+            ->willThrowException($InputException);
+
+        $this->assertEquals(
+            '',
             $this->orderIdRendererBlock->render(new DataObject(['order_id' => self::TEST_ORDER_ENTITY_ID]))
         );
     }
