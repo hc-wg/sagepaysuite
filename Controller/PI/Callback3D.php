d688b577f326076eb898da4234dbdfaf377492cb
Kevin Rinaldi <kevin@ebizmarts.com>
2020-04-22 16:19:58 -0300

    Add function doc

--- Controller/PI/Callback3D.php
+++ Controller/PI/Callback3D.php
@@ -186,6 +186,10 @@ class Callback3D extends Action implements CsrfAwareActionInterface
         return $this->cryptAndCode->decodeAndDecrypt($data);
     }
 
+    /**
+     * @param $pares
+     * @return bool
+     */
     private function isParesDuplicated($pares)
     {
         $sessionPares = $this->checkoutSession->getData(SagePaySession::PARES_SENT);
