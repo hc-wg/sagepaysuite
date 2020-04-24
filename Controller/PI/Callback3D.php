db66f83daa99b7a1fdfa6feac97a21105c4ddc64
Kevin Rinaldi <kevin@ebizmarts.com>
2020-04-24 17:05:32 -0300

    Replace LoggerInterface for SuiteLogger

--- Controller/PI/Callback3D.php
+++ Controller/PI/Callback3D.php
@@ -11,7 +11,6 @@ use Magento\Framework\App\Action\Context;
 use Magento\Framework\App\Request\InvalidRequestException;
 use Magento\Framework\App\RequestInterface;
 use Magento\Sales\Api\OrderRepositoryInterface;
-use Psr\Log\LoggerInterface;
 use Magento\Framework\App\CsrfAwareActionInterface;
 use Ebizmarts\SagePaySuite\Model\CryptAndCodeData;
 use Ebizmarts\SagePaySuite\Model\RecoverCart;
@@ -23,9 +22,6 @@ class Callback3D extends Action implements CsrfAwareActionInterface
 {
     /** @var Config */
     private $config;
-
-    /** @var LoggerInterface */
-    private $logger;
 
     private $suiteLogger;
 
@@ -51,7 +47,6 @@ class Callback3D extends Action implements CsrfAwareActionInterface
      * Callback3D constructor.
      * @param Context $context
      * @param Config $config
-     * @param LoggerInterface $logger
      * @param ThreeDSecureCallbackManagement $requester
      * @param PiRequestManagerFactory $piReqManagerFactory
      * @param OrderRepositoryInterface $orderRepository
@@ -62,7 +57,6 @@ class Callback3D extends Action implements CsrfAwareActionInterface
     public function __construct(
         Context $context,
         Config $config,
-        LoggerInterface $logger,
         ThreeDSecureCallbackManagement $requester,
         PiRequestManagerFactory $piReqManagerFactory,
         OrderRepositoryInterface $orderRepository,
@@ -74,7 +68,6 @@ class Callback3D extends Action implements CsrfAwareActionInterface
         parent::__construct($context);
         $this->config = $config;
         $this->config->setMethodCode(Config::METHOD_PI);
-        $this->logger                      = $logger;
         $this->orderRepository             = $orderRepository;
         $this->requester                   = $requester;
         $this->piRequestManagerDataFactory = $piReqManagerFactory;
@@ -87,6 +80,7 @@ class Callback3D extends Action implements CsrfAwareActionInterface
     public function execute()
     {
         try {
+            $this->suiteLogger->sageLog(Logger::LOG_REQUEST, $this->getRequest()->getPost(), [__METHOD__, __LINE__]);
             $orderId = $this->getRequest()->getParam("orderId");
             $orderId = $this->decodeAndDecrypt($orderId);
             $order = $this->orderRepository->get($orderId);
@@ -119,12 +113,12 @@ class Callback3D extends Action implements CsrfAwareActionInterface
             }
         } catch (ApiException $apiException) {
             $this->recoverCart->setShouldCancelOrder(true)->execute();
-            $this->logger->critical($apiException);
+            $this->suiteLogger->sageLog(Logger::LOG_EXCEPTION, $apiException->getTraceAsString(), [__METHOD__, __LINE__]);
             $this->messageManager->addError($apiException->getUserMessage());
             $this->javascriptRedirect('checkout/cart');
         } catch (\Exception $e) {
             $this->recoverCart->setShouldCancelOrder(true)->execute();
-            $this->logger->critical($e);
+            $this->suiteLogger->sageLog(Logger::LOG_EXCEPTION, $e->getTraceAsString(), [__METHOD__, __LINE__]);
             $this->messageManager->addError(__("Something went wrong: %1", $e->getMessage()));
             $this->javascriptRedirect('checkout/cart');
         }
