add60a8418458473c83ba86db0be9a0f6c64a930
Juan-ebizmarts <juanandres@ebizmarts.com>
2020-05-27 17:57:16 -0300

    Update test

--- Test/Unit/Controller/Server/SuccessTest.php
+++ Test/Unit/Controller/Server/SuccessTest.php
@@ -12,9 +12,7 @@ use Ebizmarts\SagePaySuite\Model\ObjectLoader\OrderLoader;
 use Magento\Checkout\Model\Session;
 use Magento\Framework\App\Action\Context;
 use Magento\Framework\App\Request\Http as HttpRequest;
-use Magento\Framework\App\Response\Http as HttpResponse;
 use Magento\Framework\Encryption\EncryptorInterface;
-use Magento\Framework\UrlInterface;
 use Magento\Quote\Model\Quote;
 use Magento\Quote\Model\QuoteRepository;
 use Magento\Sales\Model\Order;
@@ -48,17 +46,11 @@ class SuccessTest extends \PHPUnit\Framework\TestCase
     /** @var HttpRequest|\PHPUnit_Framework_MockObject_MockObject */
     private $requestMock;
 
-    /** @var HttpResponse|\PHPUnit_Framework_MockObject_MockObject */
-    private $responseMock;
-
     /** @var Success */
     private $serverSuccessController;
 
     /** @var SuiteLogger|\PHPUnit_Framework_MockObject_MockObject */
     private $suiteLoggerMock;
-
-    /** @var UrlInterface|\PHPUnit_Framework_MockObject_MockObject */
-    private $urlBuilderMock;
 
     /** @var EncryptorInterface|\PHPUnit_Framework_MockObject_MockObject */
     private $encryptorMock;
@@ -80,8 +72,6 @@ class SuccessTest extends \PHPUnit\Framework\TestCase
         $this->checkoutSessionMock = $this->getMockBuilder(Session::class)->disableOriginalConstructor()->getMock();
 
         $this->requestMock = $this->getMockBuilder(HttpRequest::class)->disableOriginalConstructor()->getMock();
-        $this->responseMock = $this->getMockBuilder(HttpResponse::class)->disableOriginalConstructor()->getMock();
-        $this->urlBuilderMock = $this->getMockBuilder(UrlInterface::class)->disableOriginalConstructor()->getMock();
         $this->messageManagerMock = $this->getMockBuilder(Manager::class)->setMethods(['addError'])->disableOriginalConstructor()->getMock();
 
         $this->orderMock = $this->getMockBuilder(Order::class)->disableOriginalConstructor()->getMock();
@@ -118,8 +108,6 @@ class SuccessTest extends \PHPUnit\Framework\TestCase
         $encrypted = '0:2:Dwn8kCUk6nZU5B7b0Xn26uYQDeLUKBrD:S72utt9n585GrslZpDp+DRpW+8dpqiu/EiCHXwfEhS0=';
 
         $this->contextMock->expects($this->once())->method('getRequest')->willReturn($this->requestMock);
-        $this->contextMock->expects($this->any())->method('getResponse')->willReturn($this->responseMock);
-        $this->contextMock->expects($this->any())->method('getUrl')->willReturn($this->urlBuilderMock);
 
         $this->requestMock->expects($this->exactly(2))->method('getParam')
             ->withConsecutive(['_store'], ['quoteid'])
@@ -170,8 +158,6 @@ class SuccessTest extends \PHPUnit\Framework\TestCase
         $encrypted = '0:2:Dwn8kCUk6nZU5B7b0Xn26uYQDeLUKBrD:S72utt9n585GrslZpDp+DRpW+8dpqiu/EiCHXwfEhS0=';
 
         $this->contextMock->expects($this->once())->method('getRequest')->willReturn($this->requestMock);
-        $this->contextMock->expects($this->any())->method('getResponse')->willReturn($this->responseMock);
-        $this->contextMock->expects($this->any())->method('getUrl')->willReturn($this->urlBuilderMock);
         $this->contextMock->expects($this->any())->method('getMessageManager')->willReturn($this->messageManagerMock);
 
         $this->requestMock->expects($this->exactly(2))->method('getParam')
@@ -215,15 +201,5 @@ class SuccessTest extends \PHPUnit\Framework\TestCase
         );
 
         $this->serverSuccessController->execute();
-    }
-
-    /**
-     * @param $body
-     */
-    private function _expectSetBody($body)
-    {
-        $this->responseMock->expects($this->once())
-            ->method('setBody')
-            ->with($body);
     }
 }
