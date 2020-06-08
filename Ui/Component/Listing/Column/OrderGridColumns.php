da7987ad587d4f1aa20d0bc4715c5be9c39e19cf
Juan-ebizmarts <juanandres@ebizmarts.com>
2020-06-08 18:08:11 -0300

    Add methods to retrieve the correct index for each integration

--- Ui/Component/Listing/Column/OrderGridColumns.php
+++ Ui/Component/Listing/Column/OrderGridColumns.php
@@ -6,7 +6,6 @@
 
 namespace Ebizmarts\SagePaySuite\Ui\Component\Listing\Column;
 
-
 class OrderGridColumns extends \Ebizmarts\SagePaySuite\Model\OrderGridInfo
 {
     const IMAGE_PATH = 'Ebizmarts_SagePaySuite::images/icon-shield-';
@@ -18,7 +17,8 @@ class OrderGridColumns extends \Ebizmarts\SagePaySuite\Model\OrderGridInfo
      */
     public function getImage(array $additional, $index)
     {
-        $status = $this->getStatus($additional, $index);
+        $integrationIndex = $this->getIndex($index);
+        $status = $this->getStatus($additional, $integrationIndex);
         if ($index == "threeDStatus") {
             $image = $this->getThreeDStatus($status);
         } else {
@@ -26,6 +26,83 @@ class OrderGridColumns extends \Ebizmarts\SagePaySuite\Model\OrderGridInfo
         }
 
         return $image;
+    }
+
+    /**
+     * @param string $index
+     * @return string
+     */
+    public function getIndex($index)
+    {
+        $paymentMethodCode = $this->getMethodCode();
+        $integrationIndex = "";
+        switch ($paymentMethodCode){
+            case 'sagepaysuiteform':
+                $integrationIndex = $this->getIndexForForm($index);
+                break;
+            case 'sagepaysuiteserver':
+            case 'sagepaysuitepaypal':
+                $integrationIndex = $this->getIndexForServerAndPayPal($index);
+                break;
+            case 'sagepaysuitepi':
+            default:
+                $integrationIndex = $index;
+                break;
+        }
+
+        return $integrationIndex;
+    }
+
+    /**
+     * @param string $index
+     * @return string
+     */
+    public function getIndexForForm($index){
+        //This function returns the index needed to use for FORM integration
+        $integrationIndex = "";
+        switch ($index){
+            case 'avsCvcCheckAddress':
+                $integrationIndex = 'AddressResult';
+                break;
+            case 'avsCvcCheckPostalCode':
+                $integrationIndex = 'PostCodeResult';
+                break;
+            case 'avsCvcCheckSecurityCode':
+                $integrationIndex = 'CV2Result';
+                break;
+            case 'threeDStatus':
+                $integrationIndex = '3DSecureStatus';
+                break;
+        }
+        error_log("Integration Index: ".$integrationIndex."\n", 3, '/Users/juan/Sites/m234/var/log/ebizmarts.log');
+
+        return $integrationIndex;
+    }
+
+    /**
+     * @param string $index
+     * @return string
+     */
+    public function getIndexForServerAndPayPal($index){
+        //This function returns the index needed to use for SERVER and PayPal integrations
+        $integrationIndex = "";
+        switch ($index){
+            case 'avsCvcCheckAddress':
+                $integrationIndex = 'AddressResult';
+                break;
+            case 'avsCvcCheckPostalCode':
+                $integrationIndex = 'PostCodeResult';
+                break;
+            case 'avsCvcCheckSecurityCode':
+                $integrationIndex = 'CV2Result';
+                break;
+            case 'threeDStatus':
+            default:
+                $integrationIndex = $index;
+                break;
+        }
+
+        return $integrationIndex;
     }
 
     /**
@@ -64,8 +141,9 @@ class OrderGridColumns extends \Ebizmarts\SagePaySuite\Model\OrderGridInfo
      */
     public function getStatusImage($status)
     {
-        $status = strtoupper($status);
-        switch($status){
+        $statusWithOutSpaces = str_replace(' ', '', $status);
+
+        switch($statusWithOutSpaces){
             case 'MATCHED':
                 $imageUrl = 'check.png';
                 break;
