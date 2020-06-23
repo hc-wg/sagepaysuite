5b4d7d05fa5989438fae022ab194f99836c3f225
Kevin Rinaldi <kevin@ebizmarts.com>
2020-06-23 17:00:57 -0300

    Fix test after values changes

--- Test/Unit/Model/Config/Source/PiChallengeWindowSizeTest.php
+++ Test/Unit/Model/Config/Source/PiChallengeWindowSizeTest.php
@@ -22,36 +22,36 @@ class PiChallengeWindowSizeTest extends \PHPUnit\Framework\TestCase
 
         $this->assertEquals(
             [
-                'value' => '01',
-                'label' => '250px x 400px',
+                'value' => 'Small',
+                'label' => __('250px x 400px'),
             ],
             $availableOptions[0]
         );
         $this->assertEquals(
             [
-                'value' => '02',
-                'label' => '390px x 400px',
+                'value' => 'Medium',
+                'label' => __('390px x 400px'),
             ],
             $availableOptions[1]
         );
         $this->assertEquals(
             [
-                'value' => '03',
-                'label' => '500px x 600px',
+                'value' => 'Large',
+                'label' => __('500px x 600px'),
             ],
             $availableOptions[2]
         );
         $this->assertEquals(
             [
-                'value' => '04',
-                'label' => '600px x 400px',
+                'value' => 'ExtraLarge',
+                'label' => __('600px x 400px'),
             ],
             $availableOptions[3]
         );
         $this->assertEquals(
             [
-                'value' => '05',
-                'label' => 'Fullscreen',
+                'value' => 'FullScreen',
+                'label' => __('Fullscreen'),
             ],
             $availableOptions[4]
         );
