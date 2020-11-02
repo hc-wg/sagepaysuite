d6c2b7a003569908cbc9c8cc6748c4455e52de31
santiagoebizmarts <santiago@ebizmarts.com>
2020-11-02 12:40:38 -0300

    log error

--- Controller/PI/Callback3Dv2.php
+++ Controller/PI/Callback3Dv2.php
@@ -128,6 +128,7 @@ class Callback3Dv2 extends Action
             $this->messageManager->addError($apiException->getUserMessage());
             $this->javascriptRedirect('checkout/cart');
         } catch (\Exception $e) {
+            var_dump($e->getMessage());
             $this->recoverCart->setShouldCancelOrder(true)->execute();
             $this->logger->critical($e);
             $this->messageManager->addError(__("Something went wrong: %1", $e->getMessage()));
