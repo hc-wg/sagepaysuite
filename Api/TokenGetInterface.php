aa102174ffa067d5001707bbfa7bf473552a2213
Kevinrx <kevin@ebizmarts.com>
2021-03-23 15:21:22 -0300

    Implement TokenGetInterface

---
+++ Api/TokenGetInterface.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Ebizmarts\SagePaySuite\Api;
+
+use Ebizmarts\SagePaySuite\Api\Data\ResultInterface;
+
+interface TokenGetInterface
+{
+    /**
+     * @param $tokenId
+     * @return \Magento\Vault\Api\Data\PaymentTokenInterface
+     */
+    public function getTokenById($tokenId);
+
+    /**
+     * @param int $customerId
+     * @return \Magento\Vault\Api\Data\PaymentTokenSearchResultsInterface[]
+     */
+    public function getTokensFromCustomer($customerId);
+
+    /**
+     * @param int $customerId
+     * @return array
+     */
+    public function getTokensFromCustomerToShowOnGrid($customerId);
+
+    /**
+     * @param string $tokenId
+     * @return string
+     */
+    public function getSagePayToken($tokenId);
+
+    /**
+     * @param string $tokenId
+     * @return ResultInterface
+     */
+    public function getSagePayTokenAsResultInterface($tokenId);
+}
