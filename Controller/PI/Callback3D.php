8cf42f1a963735202230c8dbf2201e4aadb61a24
santiagoebizmarts <santiago@ebizmarts.com>
2021-05-07 10:46:02 -0300

    Fix all use cases

--- Controller/PI/Callback3D.php
+++ Controller/PI/Callback3D.php
@@ -107,7 +107,7 @@ class Callback3D extends Action
 
             $payment = $order->getPayment();
             if ($this->isParesDuplicated($payment, $sanitizedPares)) {
-                $this->javascriptRedirect('sagepaysuite/pi/success');
+                $this->javascriptRedirect('sagepaysuite/pi/success', $order->getQuoteId());
                 return;
             } else {
                 $payment->setAdditionalInformation(SagePaySession::PARES_SENT, $sanitizedPares);
@@ -115,7 +115,7 @@ class Callback3D extends Action
             }
 
             if ($order->getState() !== \Magento\Sales\Model\Order::STATE_PENDING_PAYMENT) {
-                $this->javascriptRedirect('sagepaysuite/pi/success');
+                $this->javascriptRedirect('sagepaysuite/pi/success', $order->getQuoteId());
                 return;
             }
             /** @var \Ebizmarts\SagePaySuite\Api\Data\PiRequestManager $data */
@@ -134,7 +134,7 @@ class Callback3D extends Action
             $response = $this->requester->placeOrder();
 
             if ($response->getErrorMessage() === null) {
-                $this->javascriptRedirect('sagepaysuite/pi/success');
+                $this->javascriptRedirect('sagepaysuite/pi/success', $order->getQuoteId());
             } else {
                 $this->messageManager->addError($response->getErrorMessage());
                 $this->javascriptRedirect('checkout/cart');
@@ -168,14 +168,18 @@ class Callback3D extends Action
         return ($savedPares !== null) && ($pares === $savedPares);
     }
 
-    private function javascriptRedirect($url)
+    private function javascriptRedirect($url, $quoteId)
     {
+        $finalUrl = $this->_url->getUrl($url, ['_secure' => true]);
+        if ($quoteId !== null) {
+            $finalUrl .= "?quoteId=$quoteId";
+        }
         //redirect to success via javascript
         $this
             ->getResponse()
             ->setBody(
                 '<script>window.top.location.href = "'
-                . $this->_url->getUrl($url, ['_secure' => true])
+                . $finalUrl
                 . '";</script>'
             );
     }
